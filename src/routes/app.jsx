acf260b716ae6c471ecdd7de2bee5a1b046e3bfc
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2018-03-13 17:36:35 -0300

    Added menu items

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -6,17 +6,26 @@ import Icons from 'views/Icons/Icons';
 import Maps from 'views/Maps/Maps';
 import Notifications from 'views/Notifications/Notifications';
 import SkyBot from 'views/SkyBot/SkyBot';
+import SolarSystems from 'views/SolarSystems/SolarSystems';
+import FilterObject from 'views/FilterObject/FilterObject';
+import GetPointings from 'views/GetPointings/GetPointings';
+import ExposureDownload from 'views/ExposureDownload/ExposureDownload';
 
 const appRoutes = [
     { path: "/dashboard", name: "Dashboard", icon: "fa fa-home", component: Dashboard },
-    { path: "/skybot", name: "SkyBoT", icon: "", component: SkyBot },
-    { path: "/user", name: "User Profile", icon: "pe-7s-user", component: UserProfile },
-    { path: "/table", name: "Table List", icon: "pe-7s-note2", component: TableList },
-    { path: "/typography", name: "Typography", icon: "pe-7s-news-paper", component: Typography },
-    { path: "/icons", name: "Icons", icon: "pe-7s-science", component: Icons },
-    { path: "/maps", name: "Maps", icon: "pe-7s-map-marker", component: Maps },
-    { path: "/notifications", name: "Notifications", icon: "pe-7s-bell", component: Notifications },
-    { redirect: true, path:"/", to:"/dashboard", name: "Dashboard" }
+    { path: "/getpointings", name: "Get Pointings", icon: "pe-7s-target", component: GetPointings },
+    { path: "/solarsystems", name: "Solar Systems", icon: "pe-7s-sun", component: SolarSystems },
+    { path: "/skybot", name: "SkyBoT", icon: "pe-7s-science", component: SkyBot },
+    { path: "/filterobject", name: "Filter Objects", icon: "pe-7s-filter", component: FilterObject },
+    { path: "/exposuredownload", name: "Exposure Download", icon: "pe-7s-download", component: ExposureDownload },
+
+    // { path: "/user", name: "User Profile", icon: "pe-7s-user", component: UserProfile },
+    // { path: "/table", name: "Table List", icon: "pe-7s-note2", component: TableList },
+    // { path: "/typography", name: "Typography", icon: "pe-7s-news-paper", component: Typography },
+    // { path: "/icons", name: "Icons", icon: "pe-7s-science", component: Icons },
+    // { path: "/maps", name: "Maps", icon: "pe-7s-map-marker", component: Maps },
+    // { path: "/notifications", name: "Notifications", icon: "pe-7s-bell", component: Notifications },
+    // { redirect: true, path:"/", to:"/dashboard", name: "Dashboard" }
 ];
 
 export default appRoutes;
---
+++ src/views/SolarSystems/SolarSystems.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {
+    Grid, Row, Col, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+
+import Card from 'components/Card/Card.jsx'
+import {FormInputs} from 'components/FormInputs/FormInputs.jsx';
+import Button from 'elements/CustomButton/CustomButton.jsx';
+
+class SolarSystems extends Component {
+    render() {
+        return (
+            <div className="content">
+                <Grid fluid>
+                    <Row>
+                        <Col md={4}>
+                            <Card
+                                title="Solar Systems"
+                                category=""
+                                content={
+                                    <div/>
+                                }
+                            />
+                        </Col>
+                        <Col md={8}>
+                            <Card
+                                title="Tasks"
+                                category=""
+                                content={
+                                    <div/>
+                                }
+                            />
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default SolarSystems;
