ca9334102be6f9e1178831d134c1f5a71da25573
glaubervila <glauber.vila.verde@gmail.com>
2018-03-20 13:42:07 -0300

    Added handler to Filter Object Search

--- src/views/FilterObject/FilterObjectSearch.jsx
+++ src/views/FilterObject/FilterObjectSearch.jsx
@@ -5,14 +5,33 @@ import Button from 'elements/CustomButton/CustomButton.jsx';
 
 
 class FilterObjectSearch extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {search: ''};
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+      console.log(this.state)
+      this.setState({search: event.target.value});
+    }
+
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.search);
+      event.preventDefault();
+    }
+
     render() {
         return (
             <form inline>
                 <FormGroup>
                   <InputGroup>
-                    <FormControl type="text" placeholder="Search By Name" />
+                    <FormControl type="text" placeholder="Search By Name"
+                        value={this.state.value} onChange={this.handleChange}/>
                     <InputGroup.Button>
-                        <Button>Search</Button>
+                        <Button onClick={this.handleSubmit}>Search</Button>
                     </InputGroup.Button>
                   </InputGroup>
                 </FormGroup>
@@ -20,6 +39,7 @@ class FilterObjectSearch extends Component {
             </form>
         );
     }
+
 }
 
 export default FilterObjectSearch;
