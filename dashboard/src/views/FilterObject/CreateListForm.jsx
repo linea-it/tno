2761b69302f92e2f02897baa311e165c11e9b16e
glaubervila <glauber.vila.verde@gmail.com>
2018-05-01 13:16:51 -0300

    Created Form to save a new list

--- dashboard/src/views/FilterObject/CreateListForm.jsx
+++ dashboard/src/views/FilterObject/CreateListForm.jsx
@@ -1,29 +1,99 @@
 import React, { Component } from 'react';
-import { FormGroup, ControlLabel, Checkbox, Modal } from 'react-bootstrap';
+import { FormGroup, FormControl, ControlLabel, Checkbox, Modal } from 'react-bootstrap';
 import Button from 'elements/CustomButton/CustomButton.jsx';
 
 class CreateListForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      description: '',
+      formValid: false,
+      nameState: null,
+      descriptionState: null
+    };
+  }
+
+  validName = (e) => {
+
+    this.setState({ name: e.target.value })
+
+    if ((e.target.value.length > 40) || (e.target.value.length == '')) {
+      this.setState({ nameState: 'error' })
+    } else {
+      this.setState({ nameState: 'success' })
+    }
+
+    this.validForm();
+  };
+
+  validDescription = (e) => {
+
+    this.setState({ description: e.target.value })
+
+    if ((e.target.value.length == 0) || (e.target.value.length == '')) {
+      this.setState({ descriptionState: 'error' })
+    } else {
+      this.setState({ descriptionState: 'success' })
+    }
+
+    this.validForm()
+  };
+
+  validForm = () => {
+    if ((this.state.nameState == 'success') && (this.state.descriptionState == 'success')) {
+      this.setState({ formValid: true })
+    } else {
+      this.setState({ formValid: false })
+    }
+  };
+
+  onSubmit = (e) => {
+    console.log('onSubmit')
+  }
+
   render() {
     return (
       <Modal
           {...this.props}
         >
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Save</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Text in a modal</h4>
           <p>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            Save this result to a new table.
           </p>
-          <h4>Overflowing text to show scroll behavior</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </p>
+          <form>
+            <FormGroup
+                bsSize="small"
+                validationState={this.state.nameState}
+              >
+              <ControlLabel>Name</ControlLabel>
+              <FormControl
+                  type="text"
+                  value={this.state.name}
+                  onChange={this.validName}
+                />
+            </FormGroup>
+            <FormGroup
+              bsSize="small"
+              validationState={this.state.descriptionState}
+              >
+              <ControlLabel>Description</ControlLabel>
+              <FormControl
+                componentClass="textarea"
+                value={this.state.description}
+                onChange={this.validDescription} />
+            </FormGroup>
+          </form>
         </Modal.Body>
         <Modal.Footer>
+          <Button
+            // disabled={!this.state.formValid}
+            onClick={this.onSubmit}
+            >Submit</Button>
           <Button onClick={ this.props.onHide }>Close</Button>
         </Modal.Footer>
       </Modal>
