01ef57925dde72fab02370d73fe8ab747633c9be
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2018-06-28 11:45:20 -0300

    Created method to update the list of Astrometry Runs at every x intervals

--- dashboard/src/views/Astrometry/PraiaHistory.jsx
+++ dashboard/src/views/Astrometry/PraiaHistory.jsx
@@ -9,6 +9,7 @@ import overlayFactory from 'react-bootstrap-table2-overlay';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import { formatDateUTC, formatColumnHeader, formatStatus } from 'utils';
+import ReactInterval from 'react-interval';
 
 const columns = [
   {
@@ -63,6 +64,8 @@ class PraiaHistory extends Component {
       totalSize: 0,
       sizePerPage: 10,
       loading: false,
+      // Tempo em segundos entre cada reload da lista
+      reload_interval: 5,
     };
   }
 
@@ -96,8 +99,19 @@ class PraiaHistory extends Component {
     });
   };
 
+  reload = () => {
+    this.fetchData(this.state.page, this.state.sizePerPage);
+  };
+
   render() {
-    const { data, sizePerPage, page, totalSize, loading } = this.state;
+    const {
+      data,
+      sizePerPage,
+      page,
+      totalSize,
+      loading,
+      reload_interval,
+    } = this.state;
     const pagination = paginationFactory({
       page: page,
       sizePerPage: sizePerPage,
@@ -114,31 +128,38 @@ class PraiaHistory extends Component {
     };
 
     return (
-      <Card
-        title=""
-        category="Manage the completed Astrometry rounds"
-        content={
-          <BootstrapTable
-            striped
-            hover
-            condensed
-            remote
-            bordered={false}
-            keyField="id"
-            noDataIndication="..."
-            data={data}
-            columns={columns}
-            pagination={pagination}
-            onTableChange={this.handleTableChange}
-            loading={loading}
-            overlay={overlayFactory({
-              spinner: true,
-              background: 'rgba(192,192,192,0.3)',
-            })}
-            rowEvents={rowEvents}
-          />
-        }
-      />
+      <div>
+        <ReactInterval
+          timeout={reload_interval * 1000}
+          enabled={true}
+          callback={this.reload}
+        />
+        <Card
+          title=""
+          category="Manage the completed Astrometry rounds"
+          content={
+            <BootstrapTable
+              striped
+              hover
+              condensed
+              remote
+              bordered={false}
+              keyField="id"
+              noDataIndication="..."
+              data={data}
+              columns={columns}
+              pagination={pagination}
+              onTableChange={this.handleTableChange}
+              loading={loading}
+              overlay={overlayFactory({
+                spinner: true,
+                background: 'rgba(192,192,192,0.3)',
+              })}
+              rowEvents={rowEvents}
+            />
+          }
+        />
+      </div>
     );
   }
 }
