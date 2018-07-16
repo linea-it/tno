18851f0c996cb7b2f03ba45dbe314682b5cf8ac9
Glauber Costa Vila-Verde <glauber.vila.verde@gmail.com>
2018-07-16 13:20:25 -0300

    Update PointingApi.jsx

--- dashboard/src/views/Pointings/PointingApi.jsx
+++ dashboard/src/views/Pointings/PointingApi.jsx
@@ -5,20 +5,17 @@ class PointingApi {
     this.api = process.env.REACT_APP_API;
   }
   getPointingLists = ({ page, pageSize, search, filters }) => {
-    // console.log('CHEGAMOS AQUI E O VALOR DE ARRAYFILTER É', filters);
+
     const params = {
       page: page,
       pageSize: pageSize,
       search: search,
       //band: filters,
     };
-    // console.log('ANTES do for, este é o valor do params %o', params);
 
     filters.forEach(function(el) {
       params[el.property] = el.value;
     });
-
-    // console.log('DENTRO do for, este é o valor do params %o', params);
 
     return axios.get(`${this.api}/pointing/`, { params: params });
   };
