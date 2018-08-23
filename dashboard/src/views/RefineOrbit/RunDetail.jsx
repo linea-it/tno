71c7f2e6d5de658f370a9616450234204ef09a1a
GeorgeSued14 <georgesued.dev@gmail.com>
2018-08-23 15:01:39 -0300

    Created the stepStats, listStats, and donutStats components #100

--- dashboard/src/views/RefineOrbit/RunDetail.jsx
+++ dashboard/src/views/RefineOrbit/RunDetail.jsx
@@ -1,14 +1,15 @@
-// React e Prime React
 import React, { Component } from 'react';
 import 'primereact/resources/themes/omega/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-// Api Rest
 import OrbitApi from './OrbitApi';
 import { withRouter } from 'react-router-dom';
 import AsteroidList from './AsteroidList';
 import PropTypes from 'prop-types';
-import { Card } from 'primereact/card';
+import DonutStats from 'components/StatsCard/DonutStats.jsx';
+import ListStats from 'components/ListStats/ListStats.jsx';
+import StepStats from 'components/StepStats/StepStats.jsx';
+
 class RefineOrbitRunDetail extends Component {
   state = this.initialState;
   api = new OrbitApi();
@@ -48,22 +49,54 @@ class RefineOrbitRunDetail extends Component {
   };
 
   render() {
+    const stats = [
+      { name: 'STATUS', value: 400 },
+      { name: 'Active Projects', value: 400 },
+      { name: 'Open Tasks', value: 300 },
+      { name: 'Support Tickets', value: 200 },
+      { name: 'Active Timers', value: 300 },
+    ];
+
+    const stats2 = [
+      { name: 'Active Projects', value: 400 },
+      { name: 'Active Projects', value: 400 },
+      { name: 'Active Projects', value: 400 },
+    ];
+
+    const data = [
+      { name: 'Executado', value: 400 },
+      { name: 'Warning', value: 300 },
+      { name: 'Não dfdff', value: 200 },
+      { name: 'Fail', value: 300 },
+    ];
+    const colors = ['#1D3747', '#305D78', '#89C8F7', '#A8D7FF'];
+
     return (
-      <div className="content">
+      <div>
         <div className="ui-g">
-          <div className="ui-g-4">
-            <Card
-              title="Statistics"
-              subTitle="Curabitur id lacus est. Donec erat sapien, dignissim ut arcu sed."
-            />
+          <div className="ui-g-4 ui-md-4 ui-sm-1">
+            <ListStats Badge="STATUS" title="My Stats" data={stats} />
           </div>
-          <div className="ui-g-4">
-            <Card title="" subTitle="" />
+
+          <div className="ui-g-4 ui-md-4 ui-sm-1">
+            <div className="ui-g-4 ui-md-12 ui-sm-1">
+              <DonutStats
+                subTitle="Statistics of executation"
+                data={data}
+                fill={colors}
+              />
+            </div>
+
+            <div className="ui-g-4 ui-md-12 ui-sm-1">
+              <StepStats title="My Stats" columns={stats2} />
+            </div>
           </div>
-          <div className="ui-g-4">
-            <Card title="" subTitle="" />
+
+          <div className="ui-g-4 ui-md-4 ui-sm-1">
+            <ListStats Badge="STATUS" title="My Stats" data={stats} />
           </div>
         </div>
+
         <div className="ui-g">
           <div className="ui-g-12">
             <AsteroidList
