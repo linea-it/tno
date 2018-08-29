df1e71328364f690280a5cab19462a77460a848a
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2018-08-29 13:11:23 -0300

    minor fix

--- dashboard/src/views/Prediction/Panel.jsx
+++ dashboard/src/views/Prediction/Panel.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-// import RefineOrbitSubmit from './Submit';
-// import RefineOrbitRunning from './Running';
-// import RefineOrbitHistory from './History';
 import PredictionHistory from './History';
 
 import PropTypes from 'prop-types';
