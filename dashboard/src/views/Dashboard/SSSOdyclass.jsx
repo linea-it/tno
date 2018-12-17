a2ed076ef985a378c4f54ffb47bddddc975a1d69
GeorgeSued14 <georgesuedgs@gmail.com>
2018-12-17 13:23:12 -0200

    alter name of class

--- dashboard/src/views/Dashboard/SSSOdyclass.jsx
+++ dashboard/src/views/Dashboard/SSSOdyclass.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Content from 'components/CardContent/CardContent.jsx';
 import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+// import { scaleLog } from 'd3-scale';
 
 class SSSOdyclass extends Component {
   render() {
@@ -15,13 +16,13 @@ class SSSOdyclass extends Component {
             <BarChart
               width={350}
               height={200}
-              data={propSet.data.asteroids_by_class}
+              data={propSet.data.asteroids_by_dynclass}
               margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
             >
               <Tooltip />
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="dynclass" />
-              <YAxis dataKey="count" />
+              <YAxis scale="log" dataKey="count" />
               <Bar barSize={10} dataKey="count" fill="#3c1e7e" />;
             </BarChart>
           </div>
