53ef7bc1f43b0b9f63b143b648b913c7164fec35
pedromartinsjesus <pedromjesusti@gmail.com>
2019-08-02 17:29:53 -0300

    Inserted function to read output file

--- dashboard/src/views/Astrometry/AsteroidRunDetail.jsx
+++ dashboard/src/views/Astrometry/AsteroidRunDetail.jsx
@@ -9,8 +9,9 @@ import { Button } from 'primereact/button';
 import ListStats from 'components/Statistics/ListStats.jsx';
 import PanelCostumize from 'components/Panel/PanelCostumize';
 import { TreeTable } from 'primereact/treetable';
+import Log from './Log';
 
-const outputs = {"D00512549_z_c51_r2379p01_immasked":[],"D00512548_z_c54_r2379p01_immasked":[],"D00512547_i_c55_r2379p01_immasked":[],"D00507395_z_c23_r2379p01_immasked":[],"D00507394_z_c55_r2379p01_immasked":[],"D00507393_i_c48_r2379p01_immasked":[],"D00364726_g_c56_r2166p01_immasked":[{"catalog":null,"filename":"D00364726_g_c56_r2166p01_immasked.mes","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.mes","file_size":0,"extension":".mes"},{"catalog":null,"filename":"D00364726_g_c56_r2166p01_immasked.reg","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.reg","file_size":25492,"extension":".reg"},{"catalog":"gaia1","filename":"D00364726_g_c56_r2166p01_immasked.gaia1.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia1.rad.xy","file_size":301378,"extension":".xy"},{"catalog":"gaia2","filename":"D00364726_g_c56_r2166p01_immasked.gaia2.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia2.rad.xy","file_size":301378,"extension":".xy"},{"catalog":"gaia3","filename":"D00364726_g_c56_r2166p01_immasked.gaia3.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia3.rad.xy","file_size":301378,"extension":".xy"},{"catalog":"gaia4","filename":"D00364726_g_c56_r2166p01_immasked.gaia4.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia4.rad.xy","file_size":301378,"extension":".xy"},{"catalog":"gaia5","filename":"D00364726_g_c56_r2166p01_immasked.gaia5.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia5.rad.xy","file_size":301378,"extension":".xy"},{"catalog":"gaia6","filename":"D00364726_g_c56_r2166p01_immasked.gaia6.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia6.rad.xy","file_size":301378,"extension":".xy"},{"catalog":"gaia7","filename":"D00364726_g_c56_r2166p01_immasked.gaia7.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia7.rad.xy","file_size":301378,"extension":".xy"},{"catalog":"ucac4","filename":"D00364726_g_c56_r2166p01_immasked.ucac4.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.ucac4.rad.xy","file_size":301378,"extension":".xy"},{"catalog":"ucac5","filename":"D00364726_g_c56_r2166p01_immasked.ucac5.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.ucac5.rad.xy","file_size":301378,"extension":".xy"},{"catalog":"gaia_dr2","filename":"D00364726_g_c56_r2166p01_immasked.gaia_dr2.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia_dr2.rad.xy","file_size":301378,"extension":".xy"}],"D00503010_z_c30_r2378p01_immasked":[{"catalog":null,"filename":"D00503010_z_c30_r2378p01_immasked.mes","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.mes","file_size":0,"extension":".mes"},{"catalog":null,"filename":"D00503010_z_c30_r2378p01_immasked.reg","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.reg","file_size":33857,"extension":".reg"},{"catalog":"gaia1","filename":"D00503010_z_c30_r2378p01_immasked.gaia1.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia1.rad.xy","file_size":401280,"extension":".xy"},{"catalog":"gaia2","filename":"D00503010_z_c30_r2378p01_immasked.gaia2.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia2.rad.xy","file_size":401280,"extension":".xy"},{"catalog":"gaia3","filename":"D00503010_z_c30_r2378p01_immasked.gaia3.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia3.rad.xy","file_size":401280,"extension":".xy"},{"catalog":"gaia4","filename":"D00503010_z_c30_r2378p01_immasked.gaia4.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia4.rad.xy","file_size":401280,"extension":".xy"},{"catalog":"gaia5","filename":"D00503010_z_c30_r2378p01_immasked.gaia5.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia5.rad.xy","file_size":401280,"extension":".xy"},{"catalog":"gaia6","filename":"D00503010_z_c30_r2378p01_immasked.gaia6.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia6.rad.xy","file_size":401280,"extension":".xy"},{"catalog":"gaia7","filename":"D00503010_z_c30_r2378p01_immasked.gaia7.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia7.rad.xy","file_size":401280,"extension":".xy"},{"catalog":"ucac4","filename":"D00503010_z_c30_r2378p01_immasked.ucac4.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.ucac4.rad.xy","file_size":401280,"extension":".xy"},{"catalog":"ucac5","filename":"D00503010_z_c30_r2378p01_immasked.ucac5.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.ucac5.rad.xy","file_size":401280,"extension":".xy"},{"catalog":"gaia_dr2","filename":"D00503010_z_c30_r2378p01_immasked.gaia_dr2.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia_dr2.rad.xy","file_size":401280,"extension":".xy"}],"D00506424_z_c35_r2379p01_immasked":[{"catalog":null,"filename":"D00506424_z_c35_r2379p01_immasked.mes","filepath":"/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.mes","file_size":0,"extension":".mes"},{"catalog":null,"filename":"D00506424_z_c35_r2379p01_immasked.reg","filepath":"/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.reg","file_size":28677,"extension":".reg"},{"catalog":"gaia1","filename":"D00506424_z_c35_r2379p01_immasked.gaia1.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.gaia1.rad.xy","file_size":339416,"extension":".xy"},{"catalog":"gaia2","filename":"D00506424_z_c35_r2379p01_immasked.gaia2.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.gaia2.rad.xy","file_size":339416,"extension":".xy"},{"catalog":"gaia3","filename":"D00506424_z_c35_r2379p01_immasked.gaia3.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.gaia3.rad.xy","file_size":339416,"extension":".xy"},{"catalog":"gaia4","filename":"D00506424_z_c35_r2379p01_immasked.gaia4.rad.xy","filepath":"/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.gaia4.rad.xy","file_size":339416,"extension":".xy"}]};
+const outputs = { "D00512549_z_c51_r2379p01_immasked": [], "D00512548_z_c54_r2379p01_immasked": [], "D00512547_i_c55_r2379p01_immasked": [], "D00507395_z_c23_r2379p01_immasked": [], "D00507394_z_c55_r2379p01_immasked": [], "D00507393_i_c48_r2379p01_immasked": [], "D00364726_g_c56_r2166p01_immasked": [{ "catalog": null, "filename": "D00364726_g_c56_r2166p01_immasked.mes", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.mes", "file_size": 0, "extension": ".mes" }, { "catalog": null, "filename": "D00364726_g_c56_r2166p01_immasked.reg", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.reg", "file_size": 25492, "extension": ".reg" }, { "catalog": "gaia1", "filename": "D00364726_g_c56_r2166p01_immasked.gaia1.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia1.rad.xy", "file_size": 301378, "extension": ".xy" }, { "catalog": "gaia2", "filename": "D00364726_g_c56_r2166p01_immasked.gaia2.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia2.rad.xy", "file_size": 301378, "extension": ".xy" }, { "catalog": "gaia3", "filename": "D00364726_g_c56_r2166p01_immasked.gaia3.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia3.rad.xy", "file_size": 301378, "extension": ".xy" }, { "catalog": "gaia4", "filename": "D00364726_g_c56_r2166p01_immasked.gaia4.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia4.rad.xy", "file_size": 301378, "extension": ".xy" }, { "catalog": "gaia5", "filename": "D00364726_g_c56_r2166p01_immasked.gaia5.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia5.rad.xy", "file_size": 301378, "extension": ".xy" }, { "catalog": "gaia6", "filename": "D00364726_g_c56_r2166p01_immasked.gaia6.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia6.rad.xy", "file_size": 301378, "extension": ".xy" }, { "catalog": "gaia7", "filename": "D00364726_g_c56_r2166p01_immasked.gaia7.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia7.rad.xy", "file_size": 301378, "extension": ".xy" }, { "catalog": "ucac4", "filename": "D00364726_g_c56_r2166p01_immasked.ucac4.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.ucac4.rad.xy", "file_size": 301378, "extension": ".xy" }, { "catalog": "ucac5", "filename": "D00364726_g_c56_r2166p01_immasked.ucac5.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.ucac5.rad.xy", "file_size": 301378, "extension": ".xy" }, { "catalog": "gaia_dr2", "filename": "D00364726_g_c56_r2166p01_immasked.gaia_dr2.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00364726_g_c56_r2166p01_immasked.gaia_dr2.rad.xy", "file_size": 301378, "extension": ".xy" }], "D00503010_z_c30_r2378p01_immasked": [{ "catalog": null, "filename": "D00503010_z_c30_r2378p01_immasked.mes", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.mes", "file_size": 0, "extension": ".mes" }, { "catalog": null, "filename": "D00503010_z_c30_r2378p01_immasked.reg", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.reg", "file_size": 33857, "extension": ".reg" }, { "catalog": "gaia1", "filename": "D00503010_z_c30_r2378p01_immasked.gaia1.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia1.rad.xy", "file_size": 401280, "extension": ".xy" }, { "catalog": "gaia2", "filename": "D00503010_z_c30_r2378p01_immasked.gaia2.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia2.rad.xy", "file_size": 401280, "extension": ".xy" }, { "catalog": "gaia3", "filename": "D00503010_z_c30_r2378p01_immasked.gaia3.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia3.rad.xy", "file_size": 401280, "extension": ".xy" }, { "catalog": "gaia4", "filename": "D00503010_z_c30_r2378p01_immasked.gaia4.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia4.rad.xy", "file_size": 401280, "extension": ".xy" }, { "catalog": "gaia5", "filename": "D00503010_z_c30_r2378p01_immasked.gaia5.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia5.rad.xy", "file_size": 401280, "extension": ".xy" }, { "catalog": "gaia6", "filename": "D00503010_z_c30_r2378p01_immasked.gaia6.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia6.rad.xy", "file_size": 401280, "extension": ".xy" }, { "catalog": "gaia7", "filename": "D00503010_z_c30_r2378p01_immasked.gaia7.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia7.rad.xy", "file_size": 401280, "extension": ".xy" }, { "catalog": "ucac4", "filename": "D00503010_z_c30_r2378p01_immasked.ucac4.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.ucac4.rad.xy", "file_size": 401280, "extension": ".xy" }, { "catalog": "ucac5", "filename": "D00503010_z_c30_r2378p01_immasked.ucac5.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.ucac5.rad.xy", "file_size": 401280, "extension": ".xy" }, { "catalog": "gaia_dr2", "filename": "D00503010_z_c30_r2378p01_immasked.gaia_dr2.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00503010_z_c30_r2378p01_immasked.gaia_dr2.rad.xy", "file_size": 401280, "extension": ".xy" }], "D00506424_z_c35_r2379p01_immasked": [{ "catalog": null, "filename": "D00506424_z_c35_r2379p01_immasked.mes", "filepath": "/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.mes", "file_size": 0, "extension": ".mes" }, { "catalog": null, "filename": "D00506424_z_c35_r2379p01_immasked.reg", "filepath": "/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.reg", "file_size": 28677, "extension": ".reg" }, { "catalog": "gaia1", "filename": "D00506424_z_c35_r2379p01_immasked.gaia1.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.gaia1.rad.xy", "file_size": 339416, "extension": ".xy" }, { "catalog": "gaia2", "filename": "D00506424_z_c35_r2379p01_immasked.gaia2.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.gaia2.rad.xy", "file_size": 339416, "extension": ".xy" }, { "catalog": "gaia3", "filename": "D00506424_z_c35_r2379p01_immasked.gaia3.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.gaia3.rad.xy", "file_size": 339416, "extension": ".xy" }, { "catalog": "gaia4", "filename": "D00506424_z_c35_r2379p01_immasked.gaia4.rad.xy", "filepath": "/data/proccess/4/objects/Eris/D00506424_z_c35_r2379p01_immasked.gaia4.rad.xy", "file_size": 339416, "extension": ".xy" }] };
 
 export default class AsteroidRunDetail extends Component {
   state = this.initialState;
@@ -33,6 +34,11 @@ export default class AsteroidRunDetail extends Component {
       praiaId: 0,
       proccess: null,
       catalogName: null,
+      output_content: null,
+      log_visible: false,
+      header: null,
+      selectedNodeKey1: [],
+      expandedKeys: []
     };
   }
 
@@ -129,7 +135,7 @@ export default class AsteroidRunDetail extends Component {
             icon={this.state.download_icon}
             className="ui-button-info"
             disabled="disabled"
-            // onClick={() => this.onClickDownload(this.state.asteroid.id)}
+          // onClick={() => this.onClickDownload(this.state.asteroid.id)}
           />
         </div>
 
@@ -172,11 +178,11 @@ export default class AsteroidRunDetail extends Component {
     const filename = encodeURIComponent(rowData.filename);
     const title = encodeURIComponent(
       'Proccess: ' +
-        proccess +
-        ' of the asteroid ' +
-        rowData.asteroid +
-        '. \u00a0 File: ' +
-        rowData.filename
+      proccess +
+      ' of the asteroid ' +
+      rowData.asteroid +
+      '. \u00a0 File: ' +
+      rowData.filename
     );
 
     const history = this.props.history;
@@ -200,26 +206,26 @@ export default class AsteroidRunDetail extends Component {
   format_ccd = (ccd_name) => {
     let arr = ccd_name.split("_");
 
-     return arr[0] + " " + arr[1];
+    return arr[0] + " " + arr[1];
   };
 
-  teste = e => {
-    console.log("teste")
-    return (<div>TESTE</div>)
-  }
 
   renderOutputTreeTable = outputs => {
     const columns = [
       {
         field: 'filename',
         header: 'Filename',
-        style: {width: '30%'},
+        style: { width: '40%' },
         expander: true,
         sortable: true,
       },
       {
         field: 'catalog',
-        header: 'Reference Catalog',
+        header: 'Ref. Catalog',
+        style: {
+          width: '15%',
+          textAlign: 'center'
+        },
         sortable: true,
       },
       {
@@ -231,12 +237,16 @@ export default class AsteroidRunDetail extends Component {
         field: 'extension',
         header: 'File type',
         sortable: true,
+        style: {
+          width: '10%',
+          textAlign: 'center',
+        }
       },
       {
         field: 'actionBtn',
         style: {
           textAlign: 'center',
-          width: 80,
+          width: '10%',
         }
       }
     ];
@@ -255,14 +265,31 @@ export default class AsteroidRunDetail extends Component {
     });
     return (
       <TreeTable
-      value={outputs}
-      resizableColumns={true}
-      scrollable
-      scrollHeight="200px"
-      columnResizeMode="expand"
-    >
-    {elColumns}
-    </TreeTable>      
+        value={outputs}
+        resizableColumns={true}
+        scrollable
+        scrollHeight="200px"
+        columnResizeMode="expand"
+        expandedKeys={this.state.expandedKeys}
+        onToggle={e => {
+          console.log(e)
+          this.setState({ expandedKeys: e.value })
+        }}
+
+      //Component treeTable was updated but primereact documentation doesnt.
+      //TODO: Check previous documentation to try to solve the problem
+      // selectionMode="single"
+      // selectionKeys={this.state.selectedNodeKey1}
+      // selectionChange={e => this.setState({ selectedNodeKey1: e }, () => {
+      //   console.log(this.state.selectedNodeKey1);
+      // })}
+
+
+
+      >
+        {elColumns}
+        < Column />
+      </ TreeTable>
     );
 
   }
@@ -270,22 +297,39 @@ export default class AsteroidRunDetail extends Component {
   openFileBtn = record => {
     return (
       <Button
-        label="Back"
-        icon="fa fa-undo"
+        className="ui-button-warning"
+        icon="fa fa-file-text-o"
         onClick={() => this.handleClickOutput(record)}
       />
     )
   }
 
+
+  //Go from here
   handleClickOutput = record => {
-    console.log("handleClickOutput(%o)", record);
+
+    this.api.readOutputFile(record.filepath).then(res => {
+      const output = res.data.rows;
+      this.setState({
+        output_content: output,
+        log_visible: true,
+        header: record.filename
+      });
+    });
+
   }
+
+
+  onLogHide = () => {
+
+    this.setState({ log_visible: false, output_content: null });
+  };
 
   render() {
     const { asteroid, inputs } = this.state;
 
     const a = [];
-    
+
 
     Object.keys(outputs).forEach((ccd_name, index) => {
       const a_childrens = [];
@@ -299,6 +343,8 @@ export default class AsteroidRunDetail extends Component {
               // file_size: humanize.filesize(file.file_size),
               extension: file.extension,
               actionBtn: this.openFileBtn(file),
+              selectable: true,
+              leaf: true
             },
             children: [],
           };
@@ -375,10 +421,17 @@ export default class AsteroidRunDetail extends Component {
           </div>
           <div className="ui-g-12">
             <PanelCostumize
-              title="Inputs"
+              title="Outputs"
               content={this.renderOutputTreeTable(a)}
             />
           </div>
+          <Log
+            header={this.state.header}
+            visible={this.state.log_visible}
+            onHide={this.onLogHide}
+            content={this.state.output_content}
+            dismissableMask={true}
+          />
         </div>
       </div>
     );
