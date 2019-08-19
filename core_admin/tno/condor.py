4a667443a271cb984e8c8b8b0c38efcea7bd7e2b
Glauber Costa Vila-Verde <glauber.vila.verde@gmail.com>
2019-08-19 14:08:08 -0300

    Fixes in astrometry (#259)
    * Fix bsp filename when asteroids have a space in name
    
    * Fixed register of runs not generate output files
    
    * Handling has been implemented for jobs that are in Hold state.
    
    * Remove require cpu and memory

--- core_admin/tno/condor.py
+++ core_admin/tno/condor.py
@@ -1,12 +1,14 @@
+import json
 import logging
 from datetime import datetime
+
 import requests
 from django.conf import settings
-import json
 
 
 def get_condor_api_host():
     return "%s/%s/" % (settings.CONDOR_API.strip('/'), settings.CONDOR_CLUSTER.strip('/'))
+
 
 def check_condor_job(cluster_id, proc_id):
     logger = logging.getLogger("condor")
@@ -22,7 +24,7 @@ def check_condor_job(cluster_id, proc_id):
         })
 
         if r.status_code == requests.codes.ok:
-            logger.debug(r.json())
+            # logger.debug(r.json())
             rows = r.json()
             if len(rows) == 1:
                 return rows[0]
@@ -46,18 +48,13 @@ def check_job_history(cluster_id, proc_id):
         condor_api_jobs = "%s/%s" % (
             get_condor_api_host().strip('/'),  'history')
 
-        # projection  = ','.join([
-        #         'ClusterId', 'ProcId', 'GlobalJobId', 'JobStatus', 'JobStartDate', 'EnteredCurrentStatus',
-        #         'Args', 'Owner', 'RemoteHost', 'RequestCpus', 'RequiresWholeMachine', 'Out', 'UserLog'])
-
         r = requests.get(condor_api_jobs, params={
             'ClusterId': cluster_id,
             'ProcId': proc_id,
-            # 'cols': projection,
         })
 
         if r.status_code == requests.codes.ok:
-            logger.debug(r.json())
+            # logger.debug(r.json())
             rows = r.json()
             if len(rows) == 1:
                 return rows[0]
@@ -79,7 +76,7 @@ def submit_condor_job(payload):
         submit_api_url = "%s/%s" % (get_condor_api_host().strip('/'),
                                     'submit_job')
 
-        logger.debug("Submit API URL: %s" % submit_api_url)
+        logger.info("Submit API URL: %s" % submit_api_url)
 
         headers = {'Content-Type': 'application/json'}
 
@@ -104,3 +101,69 @@ def submit_condor_job(payload):
         logger.error(
             "Failed to submit a new job. Payload: [ %s ]" % json.dumps(payload))
         logger.error(e)
+
+
+def remove_job(clusterId, procId):
+    """
+        Remove um Job no condor pelo seu ID
+    """
+    logger = logging.getLogger("condor")
+    logger.info(
+        "Remove Job: ClusterId: [%s] ProcId: [%s]" % (clusterId, procId))
+
+    try:
+        condor_api_jobs = "%s/%s" % (
+            get_condor_api_host().strip('/'),  'remove')
+
+        r = requests.get(condor_api_jobs, params={
+            'ClusterId': clusterId,
+            'ProcId': procId,
+        })
+
+        if r.status_code == requests.codes.ok:
+            # logger.debug(r.json())
+            result = r.json()
+            if result['success']:
+                return result['job']
+            else:
+                return None
+        else:
+            logger.error(
+                "Failed to remove a job. Payload: [ %s ]" % r)
+
+    except Exception as e:
+        logger.error(e)
+        raise
+
+
+def get_job_by_id(clusterId, procId):
+    """
+        Recupera informacoes de um Job mesmo que esteja no history
+    """
+    logger = logging.getLogger("condor")
+    logger.info("get_job_by_id: ClusterId: [%s] ProcId: [%s]" % (
+        clusterId, procId))
+
+    try:
+        condor_api_jobs = "%s/%s" % (
+            get_condor_api_host().strip('/'),  'get_job')
+
+        r = requests.get(condor_api_jobs, params={
+            'ClusterId': clusterId,
+            'ProcId': procId,
+        })
+
+        if r.status_code == requests.codes.ok:
+            # logger.debug(r.json())
+            record = r.json()
+            if len(record):
+                return record
+            else:
+                return None
+        else:
+            # TODO tratar falha na requisicao
+            pass
+
+    except Exception as e:
+        logger.error(e)
+        raise
