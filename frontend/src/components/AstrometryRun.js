89b1a22302704289ed010486fb8d0c0d3bca4a58
pedromartinsjesus <pedromjesusti@gmail.com>
2019-09-17 16:06:16 -0300

    Partial creation of astrometry run template screen

--- frontend/src/components/AstrometryRun.js
+++ frontend/src/components/AstrometryRun.js
@@ -1,43 +1,77 @@
 import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Grid, Card } from '@material-ui/core';
+import { Grid, Card, CardHeader, makeStyles } from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+
+  card: {
+    marginBottom: 10,
+  }
+}));
 
 
 
-function AstrometryRun({ setTitle }) {
+function AstrometryRun({ setTitle, match: { params } }) {
 
+  const classes = useStyles();
 
   useEffect(() => {
     setTitle("Astrometry Run");
   }, []);
 
 
+
+  const runId = params ? params.id : "id";
+
   return (
 
     <div>
 
-      <Grid container>
+      <Grid container spacing={2}>
 
-        <Grid item>
+        <Grid item sm={4} xl={4}>
           <Card>
-
+            <CardHeader
+              title={`Astrometry - ${runId} `}
+            />
           </Card>
 
+
+        </Grid>
+        <Grid item sm={4} xl={4}>
+          <Card className={classes.card}>
+            <CardHeader
+              title={"Execution Statistics  "}
+            />
+          </Card>
+
+          <Card>
+            <CardHeader
+              title={"Execution Time  "}
+            />
+          </Card>
+
+
         </Grid>
 
       </Grid>
 
 
 
-      <Grid container>
+      <Grid container spacing={2}>
 
-        <Grid item>
-
+        <Grid item sm={12} xl={12}>
+          <Card className={classes.card}>
+            <CardHeader
+              title={"Asteroids"}
+            />
+          </Card>
         </Grid>
 
       </Grid>
 
-    </div>
+    </div >
 
 
 
