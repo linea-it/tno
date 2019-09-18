3b8f96c5cbe211acd7e705d2b8e9e92926edac81
pedromartinsjesus <pedromjesusti@gmail.com>
2019-09-18 11:35:46 -0300

    Partial template screen

--- frontend/src/components/AstrometryRun.js
+++ frontend/src/components/AstrometryRun.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { Grid, Card, CardHeader, makeStyles } from '@material-ui/core';
+import ListStat from './utils/CustomList';
+import { Donut } from './utils/CustomChart';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -24,6 +26,44 @@ function AstrometryRun({ setTitle, match: { params } }) {
 
   const runId = params ? params.id : "id";
 
+
+
+  const listStatus = [
+    { title: "Status", value: "value" },
+    { title: "Process", value: "value" },
+    { title: "Process Name", value: "value" },
+    { title: "Owner", value: "value" },
+    { title: "Start", value: "value" },
+    { title: "Execution", value: "value" },
+    { title: "Asteroids", value: "value" },
+    { title: "Reference Catalog", value: "value" },
+  ]
+
+
+  const donutDataStatist = [
+    { name: "Success", value: " value" },
+    { name: "Warning", value: " value" },
+    { name: "Failure", value: " value" },
+    { name: "Not Executed", value: " value" },
+    { name: "Running/Idle", value: " value" },
+  ]
+
+  const donutColorsStatistic = [
+    '#1D3747', '#305D78', '#89C8F7', '#A8D7FF', '#A8D7FF'
+  ];
+
+
+  const donutDataExecutionTime = [
+    { name: "Ccd Images", value: "value" },
+    { name: "Bsp_Jpl", value: "value" },
+    { name: "Catalog", value: "value" },
+    { name: "Astrometry", value: "value" },
+  ];
+
+  const donutColorExecutionTime = [
+    '#1D3747', '#305D78', '#89C8F7', '#A8D7FF'
+  ];
+
   return (
 
     <div>
@@ -35,6 +75,12 @@ function AstrometryRun({ setTitle, match: { params } }) {
             <CardHeader
               title={`Astrometry - ${runId} `}
             />
+            <ListStat
+              list={listStatus}
+            >
+
+            </ListStat>
+
           </Card>
 
 
@@ -44,12 +90,29 @@ function AstrometryRun({ setTitle, match: { params } }) {
             <CardHeader
               title={"Execution Statistics  "}
             />
+
+            <Donut
+              data={donutDataStatist}
+              fill={donutColorsStatistic}
+            >
+
+            </Donut>
+
           </Card>
 
           <Card>
             <CardHeader
               title={"Execution Time  "}
             />
+
+            <Donut
+              data={donutDataExecutionTime}
+              fill={donutColorExecutionTime}
+            >
+
+            </Donut>
+
+
           </Card>
 
 
