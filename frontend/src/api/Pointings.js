a11005611d7807ad2ff3aa1dda548de5391f0498
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2019-10-25 09:56:35 -0300

    Fixed API url all requests now point for same host

--- frontend/src/api/Pointings.js
+++ frontend/src/api/Pointings.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-export const url = 'http://tno-testing.linea.gov.br/api';
-
+// TODO essa variavel ja existe no auth (api) nao precisa repetir
+export const url = process.env.REACT_APP_API;
 axios.defaults.baseURL = url;
+axios.interceptors.request.use((config) => {
+  const token = 'Token 2c68c902fd81d383aca48ff17b92435f890a130d';
+  config.headers.Authorization = token;
+  return config;
+});
 
 
 export const getPointingsList = ({
