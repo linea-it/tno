939d1cfbddbfefc5592c5c38243bc965a1d0dc2f
pedromartinsjesus <pedromjesusti@gmail.com>
2019-11-13 15:18:28 -0300

    Changes on going

--- frontend/src/components/SearchSsso.js
+++ frontend/src/components/SearchSsso.js
@@ -10,6 +10,11 @@ import TextField from '@material-ui/core/TextField';
 import Slider from '@material-ui/core/Slider';
 import clsx from 'clsx';
 import Icon from '@material-ui/core/Icon';
+import Input from '@material-ui/core/Input';
+import Select from '@material-ui/core/Select';
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Chip from '@material-ui/core/Chip';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -23,6 +28,13 @@ const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
+  },
+  chips: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    margin: 2,
   },
   filterField: {
     marginLeft: theme.spacing(1),
@@ -50,6 +62,9 @@ export default function SearchSsso({ history, setTitle }) {
   const [totalCount, setTotalCount] = useState(0);
   const [vMagnitude, setVmagnitude] = useState([4, 18]);
   const [dClass, setDclass] = useState([]);
+  const [dynamicClass, setDynamicClass] = useState([0]);
+  const [sublevelDynamicClassSelected, setSublevelDynamicClassSelected] = useState([]);
+  const [sublevelDynamicClassList, setSublevelDynamicClassList] = useState([]);
 
   const classes = useStyles();
 
@@ -63,6 +78,18 @@ export default function SearchSsso({ history, setTitle }) {
   useEffect(() => {
     loadTableData();
   }, [vMagnitude, dClass]);
+
+
+  useEffect(() => {
+    let currentSublevelList = [];
+    dynamicClass.forEach((i) => {
+      const current = optionsClassFirstLevel[i];
+      const currentChildren = optionsClassSecondLevel.filter((option) => option.parentId === current.id);
+      currentSublevelList = currentSublevelList.concat(currentChildren);
+    });
+    setSublevelDynamicClassSelected(Object.keys(currentSublevelList).map((el) => Number(el)));
+    setSublevelDynamicClassList(currentSublevelList);
+  }, [dynamicClass]);
 
   const loadTableData = (event) => {
     let page = typeof event === 'undefined' ? tablePage : event.currentPage + 1;
@@ -229,6 +256,129 @@ export default function SearchSsso({ history, setTitle }) {
     }
   ]
 
+
+  const optionsClassFirstLevel = [
+    { id: 1, label: 'Centaur', value: 'Centaur' },
+    { id: 2, label: 'Hungaria', value: 'Hungaria' },
+    { id: 3, label: 'KBO', value: 'KBO' },
+    { id: 4, label: 'Mars-Crosser', value: 'Mars-Crosser' },
+    { id: 5, label: 'MB', value: 'MB' },
+    { id: 6, label: 'NEA', value: 'NEA' },
+    { id: 7, label: 'Trojan', value: 'Trojan' },
+  ];
+
+  const optionsClassSecondLevel = [
+    {
+      id: 1, parentId: 3, label: 'Detached', value: 'KBO>Detached',
+    },
+    {
+      id: 2, parentId: 3, label: 'Classical', value: 'KBO>Classical',
+    },
+    {
+      id: 3, parentId: 3, label: 'Classical>Inner', value: 'KBO>Classical>Inner',
+    },
+    {
+      id: 4, parentId: 3, label: 'Classical>Main', value: 'KBO>Classical>Main',
+    },
+    {
+      id: 5, parentId: 3, label: 'Classical>Outer', value: 'KBO>Classical>Outer',
+    },
+    {
+      id: 6, parentId: 3, label: 'Resonant>11:3', value: 'KBO>Resonant>11:3',
+    },
+    {
+      id: 7, parentId: 3, label: 'Resonant>11:6', value: 'KBO>Resonant>11:6',
+    },
+    {
+      id: 8, parentId: 3, label: 'Resonant>11:8', value: 'KBO>Resonant>11:8',
+    },
+    {
+      id: 9, parentId: 3, label: 'Resonant>19:9', value: 'KBO>Resonant>19:9',
+    },
+    {
+      id: 10, parentId: 3, label: 'Resonant>2:1', value: 'KBO>Resonant>2:1',
+    },
+    {
+      id: 11, parentId: 3, label: 'Resonant>3:1', value: 'KBO>Resonant>3:1',
+    },
+    {
+      id: 12, parentId: 3, label: 'Resonant>3:2', value: 'KBO>Resonant>3:2',
+    },
+    {
+      id: 13, parentId: 3, label: 'Resonant>4:3', value: 'KBO>Resonant>4:3',
+    },
+    {
+      id: 14, parentId: 3, label: 'Resonant>5:2', value: 'KBO>Resonant>5:2',
+    },
+    {
+      id: 15, parentId: 3, label: 'Resonant>5:3', value: 'KBO>Resonant>5:3',
+    },
+    {
+      id: 16, parentId: 3, label: 'Resonant>5:4', value: 'KBO>Resonant>5:4',
+    },
+    {
+      id: 17, parentId: 3, label: 'Resonant>7:2', value: 'KBO>Resonant>7:2',
+    },
+    {
+      id: 18, parentId: 3, label: 'Resonant>7:3', value: 'KBO>Resonant>7:3',
+    },
+    {
+      id: 19, parentId: 3, label: 'Resonant>7:4', value: 'KBO>Resonant>7:4',
+    },
+    {
+      id: 20, parentId: 3, label: 'Resonant>9:4', value: 'KBO>Resonant>9:4',
+    },
+    {
+      id: 21, parentId: 3, label: 'Resonant>9:5', value: 'KBO>Resonant>9:5',
+    },
+    {
+      id: 22, parentId: 3, label: 'SDO', value: 'KBO>SDO',
+    },
+    {
+      id: 23, parentId: 5, label: 'Cybele', value: 'MB>Cybele',
+    },
+    {
+      id: 24, parentId: 5, label: 'Hilda', value: 'MB>Hilda',
+    },
+    {
+      id: 25, parentId: 5, label: 'Inner', value: 'MB>Inner',
+    },
+    {
+      id: 26, parentId: 5, label: 'Middle', value: 'MB>Middle',
+    },
+    {
+      id: 27, parentId: 5, label: 'Outer', value: 'MB>Outer',
+    },
+    {
+      id: 28, parentId: 6, label: 'Amor', value: 'NEA>Amor',
+    },
+    {
+      id: 29, parentId: 6, label: 'Apollo', value: 'NEA>Apollo',
+    },
+    {
+      id: 30, parentId: 6, label: 'Aten', value: 'NEA>Aten',
+    },
+    {
+      id: 31, parentId: 6, label: 'Aten', value: 'NEA>Atira',
+    },
+    {
+      id: 32, parentId: 4, label: 'Deep', value: 'Mars-Crosser>Deep',
+    },
+    {
+      id: 33, parentId: 4, label: 'Shallow', value: 'Mars-Crosser>Shallow',
+    },
+  ];
+
+  const handleDynamicClass = (e) => {
+    setSublevelDynamicClassSelected([]);
+    setDynamicClass(e.target.value);
+  };
+
+  const handleSublevelDynamicClass = (e) => {
+    console.log(e.target.value);
+    setSublevelDynamicClassSelected(e.target.value);
+  };
+
   return (
     <Grid>
       <Grid container spacing={6}>
@@ -239,10 +389,10 @@ export default function SearchSsso({ history, setTitle }) {
             />
             <CardContent>
               <Toolbar>
-                <TextField
+                {/* <TextField
                   className={classes.filterField}
                   select
-                  label="Dynamics Class"
+                  label="Dynamic Class"
                   SelectProps={
                     { multiple: true }
                   }
@@ -250,7 +400,71 @@ export default function SearchSsso({ history, setTitle }) {
                   onChange={(event) => { setDclass(event.target.value); }}
                 >
                   {loadDynamicClassColumns()}
-                </TextField>
+                </TextField> */}
+
+                <div>
+                  <FormControl>
+                    <InputLabel>Dynamic class</InputLabel>
+                    <Select
+                      multiple
+                      value={dynamicClass}
+                      onChange={handleDynamicClass}
+                      className={classes.filterField}
+                      input={<Input />}
+                      renderValue={() => (
+                        <div className={classes.chips}>
+                          {dynamicClass.map((i) => (
+                            <Chip
+                              key={optionsClassFirstLevel[i].id}
+                              label={optionsClassFirstLevel[i].label}
+                              className={classes.chip}
+                            />
+                          ))}
+                        </div>
+                      )}
+                    >
+                      {optionsClassFirstLevel.map((option, i) => (
+                        <MenuItem key={option.id} value={i}>
+                          {option.label}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </div>
+
+                <div>
+                  <FormControl className={classes.formControl} fullWidth>
+                    <InputLabel>Sublevel dynamic class</InputLabel>
+                    <Select
+                      disabled={!(sublevelDynamicClassList.length > 0)}
+
+                      //  className={classes.filterField}
+                      multiple
+                      value={sublevelDynamicClassSelected}
+                      onChange={handleSublevelDynamicClass}
+                      input={<Input />}
+                      renderValue={() => (
+                        <div className={classes.chips}>
+                          {sublevelDynamicClassSelected.map((i) => (
+                            <Chip
+                              key={sublevelDynamicClassList[i].id}
+                              label={sublevelDynamicClassList[i].label}
+                              className={classes.chip}
+                            />
+                          ))}
+                        </div>
+                      )}
+                    >
+                      {sublevelDynamicClassList.map((option, i) => (
+                        <MenuItem key={option.id} value={i}>
+                          {option.label}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </div>
+
+
                 <div className={classes.filterSlider}>
                   <Typography gutterBottom variant="body2" className={classes.filterSliderLabel}>
                     {`Magnitude(g): ${vMagnitude}`}
@@ -271,6 +485,7 @@ export default function SearchSsso({ history, setTitle }) {
                 >
                   Clear
                 </Button>
+
               </Toolbar>
               <Table
                 data={tableData}
