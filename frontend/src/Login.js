7e4beb2b61c3b11d4d0d92a585cb51dc2cb802da
pedromartinsjesus <pedromjesusti@gmail.com>
2020-01-07 11:46:06 -0300

    Changes done

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -37,7 +37,7 @@ function Login({ history, setTitle }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const loginEnv = process.env.REACT_APP_INST_LOGIN ?
+  const loginEnv = process.env.REACT_APP_INST_LOGIN || process.env.REACT_APP_INST_LOGIN !== "" ?
     process.env.REACT_APP_INST_LOGIN : "";
 
   const handleUsernameChange = (e) => setUsername(e.target.value);
@@ -60,7 +60,7 @@ function Login({ history, setTitle }) {
 
 
   const handleInstitutionalLogin = () => {
-    window.location.href = window.location.origin + "/Shibboleth.sso/Login";
+    window.location.href = process.env.REACT_APP_INST_LOGIN;
   };
 
 
@@ -117,7 +117,7 @@ function Login({ history, setTitle }) {
             Sign In
           </Button>
 
-          {loginEnv === "true" ? <Button
+          {loginEnv !== "" ? <Button
             fullWidth
             variant="contained"
             color="primary"
