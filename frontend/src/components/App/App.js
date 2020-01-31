f28cdd620815f7f277eef1f711dbb253536e1003
MatheusAllein <mtsallein@gmail.com>
2020-01-31 15:30:15 -0300

    Refactoring TNO and changing folder structure

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Redirect, Route,
+} from 'react-router-dom';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from '../../theme/MaterialTheme';
+import Drawer from '../Drawer/Drawer';
+import Login from '../Login/Login';
+import { isAuthenticated } from '../../api/Auth';
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      {isAuthenticated() ? <Drawer /> : (
+        <Router>
+          <Switch>
+            <Route exact path="/login" render={() => <Login />} />
+            <Redirect to="/login" />
+          </Switch>
+        </Router>
+      )}
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
