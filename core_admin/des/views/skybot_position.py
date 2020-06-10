d4a0256abd91740407cc6df41733e1029239ff3a
glaubervila <glauber.vila.verde@gmail.com>
2020-06-10 14:41:32 -0300

    Added a ticket filter to the skybot positions associated with DES.

--- core_admin/des/views/skybot_position.py
+++ core_admin/des/views/skybot_position.py
@@ -1,12 +1,21 @@
 from rest_framework import viewsets
 from des.models import SkybotPosition
 from des.serializers import SkybotPositionSerializer
+from django_filters import rest_framework as filters
 
+
+class PositionFilter(filters.FilterSet):
+    ticket = filters.NumberFilter(field_name='position__ticket')
+
+    class Meta:
+        model = SkybotPosition
+        fields = ['id', 'position', 'exposure', 'ccd', 'ticket']
 
 class DesSkybotPositionViewSet(viewsets.ModelViewSet):
 
     queryset = SkybotPosition.objects.select_related().all()
     serializer_class = SkybotPositionSerializer
-    filter_fields = ('id', 'position', 'exposure', 'ccd')
-    ordering_fields = ('exposue', )
-    ordering = ('exposure',)
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = PositionFilter    
+    # ordering_fields = ('exposure', )
+    # ordering = ('exposure',)
