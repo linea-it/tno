76c81c82d2fa625c7730e71c448e3d07f03e9e26
glaubervila <glauber.vila.verde@gmail.com>
2020-06-15 12:45:26 -0300

    Added ordering for some attributes of the view des/skybot_positions

--- core_admin/des/views/skybot_position.py
+++ core_admin/des/views/skybot_position.py
@@ -1,11 +1,16 @@
+from django_filters import rest_framework as filters
 from rest_framework import viewsets
+
 from des.models import SkybotPosition
 from des.serializers import SkybotPositionSerializer
-from django_filters import rest_framework as filters
+
+
 class DesSkybotPositionViewSet(viewsets.ModelViewSet):
 
     queryset = SkybotPosition.objects.select_related().all()
     serializer_class = SkybotPositionSerializer
-    filter_fields = ('id', 'position', 'exposure', 'ccd', 'ticket',) 
-    ordering_fields = ('id', )
+    filter_fields = ('id', 'position', 'exposure', 'ccd', 'ticket',)
+    ordering_fields = ('id', 'position__name', 'position__number',
+                       'position__dynclass', 'position__ra', 'position__dec', 'position__mv',)
     ordering = ('id',)
+    search_fields = ('position__name', 'position__number',)
