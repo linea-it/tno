915a11a0b113f1101220b51e84a32d5b45e3c908
Glauber Costa Vila-Verde <glauber.vila.verde@gmail.com>
2023-01-30 19:23:34 -0300

    Added settings template

---
+++ local_settings_template.py
@@ -0,0 +1,42 @@
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+# Level do Log (INFO, DEBUG, WARNING, ERROR, CRITICAL)
+LOGGING_LEVEL = DEBUG
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "m=5=08^4a(il)bba)$cd%f*#wrcammi(r(q#($b$n^-jz8%+j0"
+
+# the hostname and port number of the current Server URL
+# TODO: Verificar onde se ainda é utilizado?
+HOST_URL = "//localhost"
+
+# Email Confs
+EMAIL_HOST = "smtp.linea.org.br"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
+
+# Django Admin Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "database",
+        "PORT": 5432,
+        # IF Need Schema
+        # "OPTIONS": {"options": "-c search_path=<DB_SCHEMA>,public"},
+    },
+}
+
+# Skybot Server
+# Url para o serviço do skybot sempre com a barra no final.
+# Skybot do linea: SKYBOT_SERVER=http://srvskybot.linea.org.br/webservices/skybot/
+# Skybot da Franca: SKYBOT_SERVER="http://vo.imcce.fr/webservices/skybot/"
+SKYBOT_SERVER = "http://vo.imcce.fr/webservices/skybot/"
+
+# Shibboleth Authentication
+# Habilita ou desabilita autenticação pelo shibboleth.
+# Em ambiente de desenvolvimento ou que não esteja configurado o shibboleth usar SHIBBOLETH_ENABLED = False.
+# Em ambiente de produção que já esteja configurado com shibboleth usar SHIBBOLETH_ENABLED = True.
+SHIBBOLETH_ENABLED = False
