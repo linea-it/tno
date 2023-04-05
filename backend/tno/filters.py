597fb414a2aeca3d42265b2d33835c2fa4d4efbf
Vinicius Carvalho~ <mattoscarvalho@gmail.com>
2023-04-05 10:56:52 -0300

    class OccultationFilter working

--- backend/tno/filters.py
+++ backend/tno/filters.py
@@ -1,11 +1,15 @@
 import django_filters
 from tno.models import Occultation
 
+class CharInFilter(django_filters.BaseInFilter, django_filters.CharFilter):
+    pass
+
 class OccultationFilter(django_filters.FilterSet):
     start_date = django_filters.DateTimeFilter(field_name='date_time',lookup_expr='gte')
     end_date = django_filters.DateTimeFilter(field_name='date_time',lookup_expr='lte')
     dynclass = django_filters.CharFilter(field_name='asteroid__dynclass', lookup_expr='iexact')
     base_dynclass = django_filters.CharFilter(field_name='asteroid__base_dynclass', lookup_expr='iexact')
+    name = CharInFilter(field_name='asteroid__name', lookup_expr='in')
     class Meta:
         model = Occultation
-        fields = ['start_date','end_date', 'dynclass', 'base_dynclass']
+        fields = ['start_date','end_date', 'dynclass', 'base_dynclass', 'name']
