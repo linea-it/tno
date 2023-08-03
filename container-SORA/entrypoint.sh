b9168eb1c4e3620f787676ef7d959958780d8d8f
mozaru <mozar.silva@gmail.com>
2023-08-03 17:22:22 -0300

    conteiner SORA

---
+++ container-SORA/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# if any of the commands in your code fails for any reason, the entire script fails
+set -o errexit
+# fail exit if one of your pipe command fails
+set -o pipefail
+# exits if any of your variables is not set
+set -o nounset
+
+python3.7 /opt/server/main.py
