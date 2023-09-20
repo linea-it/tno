28cb3bce65171ce9efd61f71c7b1e91cc4e4d036
rcboufleur <rcboufleur@gmail.com>
2023-09-20 16:43:10 -0300

    Omite itens da lista quando tiverem valor Null

--- frontend/src/components/List/index.js
+++ frontend/src/components/List/index.js
@@ -5,34 +5,43 @@ import clsx from 'clsx'
 import useStyles from './styles'
 
 function List({ data, height, width, align }) {
-  const classes = useStyles({ align })
+  const classes = useStyles({ align });
 
   return (
     <MuiList className={classes.root} style={{ height, width }}>
-      {data.map((item, i, arr) => (
-        <ListItem key={i} divider={i !== arr.length - 1} dense={item.dense ? item.dense : false}>
-          <ListItemText
-            primary={
-              <span title={item.tooltip ? item.tooltip : ''} className={item.tooltip ? classes.tooltip : ''}>
-                {item.title}
-                {item.tooltip ? (
-                  <sup>
-                    <Icon className={clsx(classes.tooltipIcon, 'fas', 'fa-info-circle')} />
-                  </sup>
-                ) : null}
-              </span>
-            }
-            className={classes.itemText}
-          />
-          <ListItemText
-            className={clsx(classes.itemText, classes.itemValueText)}
-            secondary={typeof item.value === 'function' ? item.value() : item.value}
-          />
-        </ListItem>
-      ))}
+      {data.map((item, i, arr) => {
+        // Verifica se item.value é uma string que contém a palavra "null" ou se o valor é "null"
+        if ((typeof item.value === 'string' && item.value.includes('null')) || item.value === 'null') {
+          return null; // Pula a renderização se "null" for encontrado na string ou se o item for "null"
+        }
+
+        return (
+          <ListItem key={i} divider={i !== arr.length - 1} dense={item.dense ? item.dense : false}>
+            <ListItemText
+              primary={
+                <span title={item.tooltip ? item.tooltip : ''} className={item.tooltip ? classes.tooltip : ''}>
+                  {item.title}
+                  {item.tooltip ? (
+                    <sup>
+                      <Icon className={clsx(classes.tooltipIcon, 'fas', 'fa-info-circle')} />
+                    </sup>
+                  ) : null}
+                </span>
+              }
+              className={classes.itemText}
+            />
+            <ListItemText
+              className={clsx(classes.itemText, classes.itemValueText)}
+              secondary={typeof item.value === 'function' ? item.value() : item.value}
+            />
+          </ListItem>
+        );
+      })}
     </MuiList>
-  )
+  );
 }
+
+
 
 List.defaultProps = {
   height: 'auto',
