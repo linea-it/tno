222f46d7f70e606687801e5aa889e5103892074c
glaubervila <glauber.vila.verde@gmail.com>
2024-02-20 18:42:56 +0000

    Fixed lint issues

--- predict_occultation/src/run_pred_occ.py
+++ predict_occultation/src/run_pred_occ.py
@@ -22,7 +22,6 @@ from dao import (
 
 try:
     from parsl_config import get_config
-
     from predict_occultation.app import run_pipeline
 except Exception as error:
     print("Error: %s" % str(error))
