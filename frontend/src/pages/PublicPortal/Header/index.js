51bd3ef9058f6876caf1ea22904bcecfa27a568d
glaubervila <glauber.vila.verde@gmail.com>
2024-03-06 15:09:31 +0000

    Fixed bug in devcontainer with mkdocs

--- frontend/src/pages/PublicPortal/Header/index.js
+++ frontend/src/pages/PublicPortal/Header/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Button from '@mui/material/Button'
