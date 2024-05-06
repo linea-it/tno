75d7834910f4ab00335eaaa19963c95117fbbb30
josiane-silwa <josiannesilwa@gmail.com>
2024-05-05 23:13:56 -0300

    Correcao da funcao saveEmailSubscription que envia os parametros para a api subscription e salva os dados no banco de dados

--- frontend/src/services/api/Subscription.js
+++ frontend/src/services/api/Subscription.js
@@ -1,10 +1,13 @@
 import { api } from './Api'
 
-export const saveEmailSubscription = ({ email }) => {
-    const params = {
-      email
-    }
-  
-    return api.post('/subscription/', { params })
+export const saveEmailSubscription = (email) => {
+    const params = { email: email}
+    //const params = { email: "antonio@gmail.com"}
+    console.log("passando pela api")
+    //return api.post('/subscription/',{
+    //  email: "antonio@gmail.com"
+    //}).then((res) => res.data.results)
+    return api.post('/subscription/', params ).then((res) => res.data.results)
   }
 
+ 
