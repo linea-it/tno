45db18f540e631654ed1bceb7490607590ac049b
josiane-silwa <josiannesilwa@gmail.com>
2024-05-14 21:24:07 -0300

    Correção na funcao que verifica se o email digitado existe

--- frontend/src/components/Subscription/index.js
+++ frontend/src/components/Subscription/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-expressions 
 import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -12,6 +13,8 @@ import AlertTitle from '@mui/material/AlertTitle'
 import axios from 'axios'
 import {saveEmailSubscription} from '../../services/api/Subscription'
 import { data } from '../../../node_modules/browserslist/index';
+import { object } from 'prop-types';
+import { array } from '../../../node_modules/i/lib/util';
 
 const defaultTheme = createTheme();
 
@@ -28,35 +31,67 @@ export default function Subscribe() {
     const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(event.target.value)
     setValidEmail(isValid)
   }
-   
+
+  // verifica se o email digitado exite no banco
+  /*const checkEmail = () => {
+      const emails_bd = 
+        axios.get(`${window.location.origin}/api/subscription/{{ r }}`)
+        //const data = response.data
+        
+        
+        //console.log(error)
+        console.log('Já veriquei o email....')
+        //setSnackbarOpen(true)
+        //setOpen('warning')
+        //setEmail(" ")
+        return emails_bd
+  }*/
+  //checkEmail()
+  //console.log(emails_bd)
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     const r_email = data.get('email')
-    saveEmailSubscription(r_email).then(() => {
-      setSnackbarOpen(true)
-      setOpen('success')
-    })
-    axios
-      .get(`${window.location.origin}/api/subscription/`)
-      .then((res) => checkEmail(res.data))
-      setSnackbarOpen(true)
-      setOpen('warning')
-      setEmail(" ") 
-  };
+    
+    axios.get(`${window.location.origin}/api/subscription/`).then(function (resposta){
+      
+      const  tamanho = resposta.data.results.length
+      console.log(tamanho)
+      //cores.forEach(function (cor) {
+        //  console.log(cor);
+        //})
+        //const campo_email = [ ]
+        for(let i = 0; i < tamanho; i = i + 1 ) {
+          const campo_email = Object.getOwnPropertyDescriptor(resposta.data.results[i], "email").value
+        
+          if (r_email == campo_email){
+            console.log(r_email, campo_email),
+            setSnackbarOpen(true),
+            setOpen('warning'),
+            console.log('Entrei no if email existe....')
+          }else{
+            saveEmailSubscription(r_email).then(() => {
+              setSnackbarOpen(true)
+              setOpen('success')
+              console.log('salvando email no bd')})
+          } 
 
-  const checkEmail = (getdata) => {
-    console.log(getdata);
-    if(getdata.includes(email)){
-        setSnackbarOpen(true)
-        setOpen('warning')
-      }else{
-        axios.post(`${window.location.origin}/api/subscription/`, data).then((res) => {
-        console.log(res.data);
-    });
-    }
-  };
-  
+        /*************************************  dont work */
+        /*eslint no-unused-expressions: ["error", { "allowTernary": true }]*/
+        /*r_email == campo_email 
+        ? (console.log(r_email, campo_email), 
+          setSnackbarOpen(true),
+          setOpen('warning'),
+          console.log('Entrei no if email existe....'))
+        : saveEmailSubscription(r_email).then(() => {
+            setSnackbarOpen(true)
+            setOpen('success')
+            console.log('salvando email no bd')
+          })*/
+      }
+    })
+  }
+
   const handleCloseSnackbar = (event, reason) => {
     if (reason === 'clickaway') {
       return
@@ -72,6 +107,7 @@ export default function Subscribe() {
         <Box className='container textBanner'
           sx={{
             marginTop: 4,
+            mx: 'auto',
             display: 'flex',
             flexDirection: 'columns',
             alignItems: 'center',
@@ -81,8 +117,53 @@ export default function Subscribe() {
         >
            <Box component="form" onSubmit={handleSubmit} autoComplete='off' noValidate sx={{ mt: 1 , display: 'block'}}>
           <Stack direction='row' justifyContent='flex-end' alignItems='center' spacing={1}>
-
-          </Stack>
+          <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus sx={{ input: { borderRadius: '6px', height:'8px', color: 'white' }}}
+              value={email}
+              onChange={emailChangeHandler}
+              error={!validEmail}
+              helperText={!validEmail ? 'Please enter a valid email address' : ''}
+              InputLabelProps={{ sx: { color: 'white' } }}
+            />
+            <Button
+              type="submit"
+              fullWidth 
+              variant="contained"
+              sx={{ width: '20vw', height:'34px'}}
+            >
+              Subscribe
+            </Button>
+            </Stack>
+              <p>
+                Receive reports in your email with the main star occultation predictions in your region.
+              </p>
+              <Snackbar
+              anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+              open={open === 'success' || open === 'warning'}
+              autoHideDuration={3500}
+              onClose={handleCloseSnackbar}
+            >
+              <Alert
+                onClose={handleCloseSnackbar}
+                severity={open === 'success' ? 'success' : 'warning'}
+              >
+                <AlertTitle>
+                  {open === 'success' ? 'Success' : 'Warning'}
+                </AlertTitle>
+                <p>
+                  {open === 'success'
+                    ? 'Email successfully registered. Check your email inbox.'
+                    : 'Subscription with this Email already exists.'}
+                </p>
+              </Alert>
+            </Snackbar>
           </Box>
         </Box>
         </Container>
