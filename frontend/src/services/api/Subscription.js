103617e473c8bd3d320a5ba85e4f541492cb8254
josiane-silwa <josiannesilwa@gmail.com>
2024-05-24 12:02:44 -0300

    Inclusao da função que busca os emails cadastrados na api/services/Subscription

--- frontend/src/services/api/Subscription.js
+++ frontend/src/services/api/Subscription.js
@@ -1,5 +1,10 @@
 import { api } from './Api'
 
+export const getEmailDb = () => {
+  return api.get(`${window.location.origin}/api/subscription/`).then((res) => res.data.results)
+  //return api.get(`/occultations/highlights_unique_asteroids/`).then((res) => res.data)
+}
+
 export const saveEmailSubscription = (email) => {
     const params = { email: email}
     //    params = { email: "antonio@gmail.com"}
