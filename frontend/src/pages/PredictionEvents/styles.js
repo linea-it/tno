54cc9d3b8c51b58b88234ac803200bf52d9e3207
josiane-silwa <josiannesilwa@gmail.com>
2024-05-31 15:44:45 -0300

    Correção no layout da pagina de detalhes do PredictionEvents

--- frontend/src/pages/PredictionEvents/styles.js
+++ frontend/src/pages/PredictionEvents/styles.js
@@ -3,33 +3,25 @@ import { makeStyles } from '@mui/styles'
 const useStyles = makeStyles((theme) => ({
   listDetailCircunstances: {
     [theme.breakpoints.between(900, 945)]: {
-      color: 'red',
+      //color: 'red',
       width: '430px',
-      marginLeft: '-10px',             // secondary
-      //margin: '-10px 0'
+      marginLeft: '-10px'
     }
   },
 
   listDetailObject: {
     [theme.breakpoints.between(900, 945)]: {
-      color: 'blue',
-      //marginLeft: theme.spacing(-3)
+      //color: 'blue',
       width: '330px',
-      //marginLeft: '12px',
-      marginLeft: '30px'             // secondary
-      //flex: '0 2'
-      //[theme.spacing(14)]
+      marginLeft: '30px' 
     },
     [theme.breakpoints.between(945, 1100)]: {
-      color: 'pink',
-      //marginLeft: theme.spacing(-3)
+      //color: 'pink',
       width: '333px',
-      //marginLeft: '46px'
       margin: 'auto'
     },
     [theme.breakpoints.between(1100, 1175)]: {
-      color: 'pink',
-      //marginLeft: theme.spacing(-3)
+      //color: 'pink',
       width: '530px',
       marginLeft: '-18px'
     }
