3da54fe6f0a5fd348f2b479d0f8e8e68686af3f2
josiane-silwa <josiannesilwa@gmail.com>
2024-06-05 18:22:57 -0300

    Inclusao de função no index do pages Banner, que verifica se o ambiente é de desenvolvimento e retorna um alert informando isso oao usuário

--- frontend/src/pages/PublicPortal/Banner/index.js
+++ frontend/src/pages/PublicPortal/Banner/index.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Grid from '@mui/material/Grid'
 import styles from './styles'
 import Box from '@mui/material/Box'
 import { Alert } from '../../../../node_modules/@mui/lab/index'
+import { pageTnoDev } from '../../../services/api/Auth' 
+import { Typography } from '../../../../node_modules/@mui/material/index'
 
 function PublicBanner() {
   const classes = styles()
+  const [resultsDev, setResultsDev] = useState([])
+
+  useEffect(() => {
+    // Get results by year
+    pageTnoDev()
+      .then((res) => {
+        setResultsDev(res)
+      })
+      .catch(() => {
+        setResultsDev([])
+      })
+  }, [])
+  const verifyPage = resultsDev[0]
+  console.log(verifyPage)
 
   return (
     <Box className={classes.root}>
       <Grid container direction='row' justifyContent='space-between' spacing={2} className={classes.container}>
+        {verifyPage === 'Development' ? 
         <Grid item xs={12}>
           <Alert severity='warning'> 
-            YOU ARE VISITING A DEVELOPMENT PAGE.
+            YOU ARE VISITING A DEVELOPMENT PAGE. <a href="https://linea.org.br/" target='blank' rel="noopener noreferrer">
+              PLEASE VISIT OUR OFFICIAL PAGE.
+            </a>
           </Alert>
         </Grid>
+        :
+        ' '
+        }
+        
         <img src={`${process.env.PUBLIC_URL}/img/tno_logo_projetos.png`} alt='Data Release Interface' className={classes.logo} />
         <Grid item xs={12} className={classes.titleWrapper}>
           <h1 className={classes.title}>LIneA Occultation Prediction Database</h1>
