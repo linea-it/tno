a8065561bd2ebe8b123d91d831a923a9853728b6
josiane-silwa <josiannesilwa@gmail.com>
2024-06-17 15:29:31 -0300

    Criação do arquivo responsavel pela leitura das opçoes de recebimento da newsletter

---
+++ frontend/src/components/Newsletter/FrequencyTypeSelect.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import InputLabel from '@mui/material/InputLabel'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+
+function FrequencyTypeSelect({ value, onChange }) {
+  return (
+    <FormControl size='normal' fullWidth>
+      <InputLabel id='frequency-type-select-label'>Frequency</InputLabel>
+      <Select labelId='frequency-type-select-label' id='frequency-type-select' value={value} label='Frequency' onChange={onChange}>
+        <MenuItem value={1}>Monthly</MenuItem>
+        <MenuItem value={2}>Weekly</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+FrequencyTypeSelect.defaultProps = {
+  value: 1 //'Monthly'
+}
+/*
+FrequencyTypeSelect.propTypes = {
+  value: PropTypes.number.isRequired, //PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+}
+*/
+export default FrequencyTypeSelect
