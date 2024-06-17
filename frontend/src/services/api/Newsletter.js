e26e75584474366f8176f5e6ad889d61e3e5ceaf
josiane-silwa <josiannesilwa@gmail.com>
2024-06-17 15:31:16 -0300

    Inclusao da funcao para salvar os paramentros de seleções do filtro da newsletter no banco de dados da api

--- frontend/src/services/api/Newsletter.js
+++ frontend/src/services/api/Newsletter.js
@@ -8,10 +8,32 @@ export const reactivateSubscription = (id) => api.post(`/subscription/reactivate
 
 export const listPreferenceEventFilters = ({ queryKey }) => {
   const params = queryKey[1]
+  
   const { subscriptionId } = params
   if (!subscriptionId) {
     return
   }
 
   return api.get(`newsletter/preferences`, { subscription_id: subscriptionId }).then((res) => res.data)
+}
+
+// completar todos os paramentros de entrada da tabela
+export const saveListPreferenceEventFilters = ({ 
+  subscriptionId, 
+  filter_name,
+  frequency, 
+  filter_type, 
+  filter_value 
+}) => {
+  const params = { 
+    subscription_id: subscriptionId, 
+    filter_name: filter_name,
+    frequency: frequency, 
+    filter_type: filter_type,
+    filter_value: filter_value
+ }
+  
+  console.log("passando pela api saveListPreferenceEventFilters")
+  console.log(params)
+  return api.post(`newsletter/preferences/`, params )//.then((res) => res.data.results)
 }
