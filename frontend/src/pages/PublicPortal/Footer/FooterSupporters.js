7051a57b12e6aadc5b90a3541ce98d3fbe0611af
jandsonrj <jandson1512@gmail.com>
2024-08-30 15:00:48 -0300

    Default website footer added

--- frontend/src/pages/PublicPortal/Footer/FooterSupporters.js
+++ frontend/src/pages/PublicPortal/Footer/FooterSupporters.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import PublicSupporters from '../../../pages/PublicPortal/Home/partials/Supporters/index'
 import PublicFooter from './index'
 
 function FooterSupporters() {
   return (
     <div>
-      <PublicSupporters />
       <PublicFooter />
     </div>
   )
