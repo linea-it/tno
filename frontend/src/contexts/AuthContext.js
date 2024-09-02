6d6e16abdb4bbb2156f011bd50189ea6812055e7
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2024-09-02 18:42:34 +0000

    fix(Auth): hided login button when subscription is disabled

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -2,6 +2,7 @@
 import { createContext, useEffect, useState, useContext } from 'react'
 import { parseCookies, destroyCookie } from 'nookies'
 import { loggedUser, urlLogin, urlLogout } from '../services/api/Auth.js'
+import { environmentSettings } from '../services/api/Api.js'
 import PropTypes from 'prop-types'
 
 export const AuthContext = createContext({})
@@ -9,29 +10,43 @@ export const AuthContext = createContext({})
 // Baseado neste exemplo: https://www.mikealche.com/software-development/how-to-implement-authentication-in-next-js-without-third-party-libraries
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null)
+  const [envSettings, setEnvSettings] = useState({
+    NEWSLETTER_SUBSCRIPTION_ENABLED: false
+  })
+  // const { 'tno.csrftoken': csrftoken } = parseCookies()
 
-  const { 'tno.csrftoken': csrftoken } = parseCookies()
-
-  const isAuthenticated = !!csrftoken
+  const isAuthenticated = !!user
 
   useEffect(() => {
-    if (csrftoken) {
-      // Carrega os dados do usuario logo apos o login
-      loggedUser()
-        .then((u) => {
-          // Só permite o acesso ao dashboard se o usuario tiver o atributo user.profile.dashboard = true
-          if (u.dashboard === true) {
-            // Evita que no primeiro render do index o nome de usuario esteja em branco
-            setUser(u)
-          } else {
-            logout()
-          }
-        })
-        .catch((res) => {
-          logout()
-        })
-    }
-  }, [csrftoken])
+    // Carrega os dados do usuario
+    loggedUser()
+      .then((u) => {
+        setUser(u)
+      })
+      .catch((res) => {
+        setUser(null)
+        // logout()
+      })
+  }, [])
+
+  // useEffect(() => {
+  //   if (csrftoken) {
+  //     // Carrega os dados do usuario logo apos o login
+  //     loggedUser()
+  //       .then((u) => {
+  //         // Só permite o acesso ao dashboard se o usuario tiver o atributo user.profile.dashboard = true
+  //         if (u.dashboard === true) {
+  //           // Evita que no primeiro render do index o nome de usuario esteja em branco
+  //           setUser(u)
+  //         } else {
+  //           logout()
+  //         }
+  //       })
+  //       .catch((res) => {
+  //         logout()
+  //       })
+  //   }
+  // }, [csrftoken])
 
   async function signIn() {
     window.location.replace(urlLogin)
@@ -48,7 +63,17 @@ export function AuthProvider({ children }) {
     window.location.replace(urlLogout)
   }
 
-  return <AuthContext.Provider value={{ user: user, isAuthenticated, signIn, logout }}>{children}</AuthContext.Provider>
+  useEffect(() => {
+    environmentSettings()
+      .then((res) => {
+        setEnvSettings(res)
+      })
+      .catch(() => {
+        // TODO: Aviso de erro
+      })
+  }, [])
+
+  return <AuthContext.Provider value={{ user: user, isAuthenticated, signIn, logout, envSettings }}>{children}</AuthContext.Provider>
 }
 
 AuthProvider.propTypes = {
