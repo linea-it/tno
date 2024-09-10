43738fd94cb5dff27438919660bb0a816e8461e4
josiane-silwa <josiannesilwa@gmail.com>
2024-09-10 20:05:06 +0000

    refactor(frontend/src/pages/PredictionEvents/partials/OcultationMap/index.js): Inclusao dos pontos menores na linha central do mapa, inclusao do plugin para desenhar as sombras

--- frontend/src/pages/PredictionEvents/partials/OcultationMap/index.js
+++ frontend/src/pages/PredictionEvents/partials/OcultationMap/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useQuery } from 'react-query'
-//import L, { polyline } from 'leaflet'
+import L from 'leaflet'
 //import { MapContainer, TileLayer, Marker, Popup, Polyline, Circle, useMap, useMapEvents } from 'react-leaflet'
-import { MapContainer, TileLayer, Popup, Polyline, Circle } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup, Polyline, Circle, CircleMarker, Marker } from 'react-leaflet'
 //import sun from './data/img/sun.png'
-//import star from './data/img/estrela-pontiaguda.png'
+import star from './data/img/estrela-pontiaguda.png'
 import styles from './map.module.css'
 //import styles from './styles'
 import './leaflet.css'
 import { Box, Card } from '@mui/material'
 import { getOccultationPaths } from '../../../../services/api/Occultation'
+//import { circle } from 'leaflet'
+import { NightRegion } from 'react-leaflet-night-region'
 
 const PredictOccultationMap = ({ occultationId }) => {
   const [force, setForce] = React.useState(false)
@@ -27,10 +29,9 @@ const PredictOccultationMap = ({ occultationId }) => {
     staleTime: 1 * 60 * 1000
   })
 
-  // ponto central de plot do mapa
   let state = {
-    lat: -20.1789, //latt[0]?.min_latitude, //latsin[0], //, //lat[0][0],
-    lgt: 20.6435, //lat[9999][0],
+    lat: (data?.min_latitude + data?.max_latitude) / 2, //center.lat, //-20.1789,
+    lon: (data?.min_longitude + data?.max_longitude) / 2, //center.lon, //20.6435,
     zoom: 3.1
   }
 
@@ -49,24 +50,33 @@ const PredictOccultationMap = ({ occultationId }) => {
   })*/
 
   // definicao do icone
-  /*
+
   const starIcon = L.icon({
     iconUrl: star,
     //shadowUrl: leafShadow,
-    iconSize: [38, 38], // size of the icon
+    iconSize: [18, 18], // size of the icon
     shadowSize: [50, 64], // size of the shadow
     iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
     shadowAnchor: [4, 62], // the same for the shadow
     popupAnchor: [-3, -76]
     //lat: [-47.1729,-10.0068],
     //lng: [-27.2729,-13.9068],
-  })*/
+  })
 
-  const circle1Options = {
+  const circleOptions = {
     color: 'blue',
-    fillColor: 'blue',
+    //fillColor: 'blue',
     //opacity: 100
+    //stroke: 'false',
     weight: 20
+    //radius: 500,
+  }
+  const circleminOptions = {
+    color: 'blue',
+    //fillColor: 'blue',
+    //opacity: 100
+    stroke: 'true',
+    weight: 10
     //radius: 500,
   }
 
@@ -93,19 +103,27 @@ const PredictOccultationMap = ({ occultationId }) => {
   //arrays das linhas central upper e lower
   let lineDown = [],
     lineCenter = [],
+    pointlineCenter = [],
     lineUpper = []
 
   const latLonSize = data?.uncertainty_lower_limit_latitude.length
 
   for (let i = 0; i < latLonSize; i++) {
-    //console.log(i, data?.uncertainty_lower_limit_latitude[i], data?.uncertainty_lower_limit_longitude[i])
     lineUpper[i] = [data?.uncertainty_upper_limit_latitude[i], data?.uncertainty_upper_limit_longitude[i]]
     lineCenter[i] = [data?.central_path_latitude[i], data?.central_path_longitude[i]]
     lineDown[i] = [data?.uncertainty_lower_limit_latitude[i], data?.uncertainty_lower_limit_longitude[i]]
   }
 
+  for (let i = 0; i < latLonSize; i += 64) {
+    pointlineCenter[i] = [data?.central_path_latitude[i], data?.central_path_longitude[i]]
+  }
+
+  // desenha as sobras
+  const shadow = false //true
+
   console.log('dataall', data)
-  console.log('linecenter', lineCenter)
+  //console.log('pointlinecenter', pointlineCenter)
+  //console.log('linecenter', lineCenter)
 
   return (
     <Card className={styles.mapPrint} spacing={4}>
@@ -115,24 +133,37 @@ const PredictOccultationMap = ({ occultationId }) => {
           (
             <MapContainer
               className={styles.map}
-              center={[data?.central_path_latitude[600], data?.central_path_longitude[600]]}
+              //center={[data?.central_path_latitude[600], data?.central_path_longitude[600]]}
+              center={[state.lat, state.lon]}
               zoom={state.zoom}
             >
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
               />
+              {shadow === true ? (
+                <NightRegion
+                  //fillColor='#00345c'
+                  weight='0.01'
+                  color='#001a2e'
+                  refreshInterval={6000} // custom refresh rate in milliseconds, default set to 5000ms
+                />
+              ) : (
+                ''
+              )}
+
               {/*<MyComponent /> // pega dimensoes do zoom map*/}
-              <Polyline pathOptions={traceOptions} positions={lineUpper}>
-                <Popup>I am a line blue</Popup>
-              </Polyline>
-              <Polyline pathOptions={blueOptions} positions={lineCenter}>
-                <Popup>I am a line blue</Popup>
-              </Polyline>
-              <Polyline pathOptions={traceOptions} positions={lineDown}>
-                <Popup>I am a line yellow</Popup>
-              </Polyline>
-              <Circle center={lineCenter[600]} pathOptions={circle1Options} radius={60000} />
+              <Polyline pathOptions={traceOptions} positions={lineUpper}></Polyline>
+              <Polyline pathOptions={blueOptions} positions={lineCenter}></Polyline>
+              <Polyline pathOptions={traceOptions} positions={lineDown}></Polyline>
+              {/*console.log('estrela', lineCenter[777]) vertical, horizontal*/}
+              <Marker position={[-35.338527765322894, -36.52870116703535]} icon={starIcon}>
+                <Popup>I am a star </Popup>
+              </Marker>
+              <Circle center={lineCenter[637]} pathOptions={circleOptions} radius={data?.diameter} />
+              {pointlineCenter.map((point) => (
+                <CircleMarker center={point} pathOptions={circleminOptions} radius={1} />
+              ))}
             </MapContainer>
           ))}
       </Box>
