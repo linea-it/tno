3009031f573e938320dec81123079c1906dbb194
Rodrigo Boufleur <rcboufleur@gmail.com>
2024-10-03 15:39:21 -0300

    filtro por magnitude e local_solar_time implementados

--- backend/newsletter/process_event_filter.py
+++ backend/newsletter/process_event_filter.py
@@ -6,6 +6,7 @@ import colorlog
 from newsletter.models import EventFilter
 
 from tno.models import Occultation
+from django.db.models import Q
 
 
 class ProcessEventFilters:
@@ -42,9 +43,47 @@ class ProcessEventFilters:
             date_start = datetime.fromisoformat(date_start).astimezone(tz=timezone.utc)
             date_end = datetime.fromisoformat(date_end).astimezone(tz=timezone.utc)
 
+            # query baseado no intervalo temporal
+            #TODO: adaptar isso ao intervalo weekly e monthly
             query_occultation = Occultation.objects.filter(
                 date_time__range=[date_start, date_end]
             )
+
+            #se magmax e magmin foram definidos, filtra por magnitude
+            magmax = input.get("magnitude_max", None)
+            magmin = input.get("magnitude_min", None)
+            if magmax and magmin:
+                query_occultation = query_occultation.filter(
+                    g_star__range=[magmin, magmax]
+                )
+
+            #TODO: Se filter_type foi definido, filtra por nome, classe ou subclasse
+            #TODO: se magdrop foi definido, filtra por magdrop
+
+            # se local_solar_time foi definido
+            local_solar_time_after = input.get("local_solar_time_after", None)
+            local_solar_time_before = input.get("local_solar_time_before", None)
+
+            if local_solar_time_after and local_solar_time_before:
+                datetime_after = datetime.combine(datetime.min, local_solar_time_after)
+                datetime_before = datetime.combine(datetime.min, local_solar_time_before)
+                time_difference = datetime_before - datetime_after
+
+                if time_difference.seconds >= 0:
+                    query_occultation = query_occultation.filter(
+                        loc_t__range=[local_solar_time_after, local_solar_time_before]
+                    )
+                else:
+                    after = Q(loc_t__gte=local_solar_time_after, loc_t__lte=time(23, 59, 59.999999))
+                    before = Q(loc_t__gte=time(0, 0, 0), loc_t__lte=local_solar_time_before)
+                    query_occultation = query_occultation.filter(after | before)
+                    
+            #TODO: se event_duration foi definido
+
+            #TODO: se diameter_min e diameter_max foram definidos, filtra por diametro
+
+            #TODO: filtra por geofiltro (latitude, longitude, location_radius)
+
             # print(query_occultation.values()[0])
             if query_occultation:
                 return query_occultation
