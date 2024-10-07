171165ea0ba801315c8e1499bec907990a0060f6
josiane-silwa <josiannesilwa@gmail.com>
2024-10-07 20:20:53 +0000

    refactor(backend/newsletter/process_event_filter.py): Implementação dos filtros por local_solar_time, event_duration, diametro, magnitude, latitude e longitude

--- backend/newsletter/process_event_filter.py
+++ backend/newsletter/process_event_filter.py
@@ -3,10 +3,10 @@ import sys
 from datetime import datetime, time, timezone
 
 import colorlog
+from django.db.models import Q
 from newsletter.models import EventFilter
 
 from tno.models import Occultation
-from django.db.models import Q
 
 
 class ProcessEventFilters:
@@ -37,54 +37,124 @@ class ProcessEventFilters:
             # filtro por data. Obs, os filtros salvos nao tem data
             # a data será passada no momento de fazer a rodada
             # por exemplo, periodo weekly = data + 7 dias
-            # periodo month = data e o mes atual
+            # periodo month = data + 30 dias
+            """
+            period = input.get('frequency', None)
 
-            date_end = "2025-03-12 15:01:15"
+            if period == 1:
+                date_end = "2024-04-12 15:01:15"
+                # date_p = date_start + 7
+                print("periodo monthly", period, "data final", date_end)
+            else:
+                # date_end = date_end + 30
+                date_end = "2024-12-12 15:01:15"
+                print("periodo  weekly", period, "data final", date_end)
+            """
+            date_end = "2024-12-12 15:01:15"
             date_start = datetime.fromisoformat(date_start).astimezone(tz=timezone.utc)
             date_end = datetime.fromisoformat(date_end).astimezone(tz=timezone.utc)
 
             # query baseado no intervalo temporal
-            #TODO: adaptar isso ao intervalo weekly e monthly
+            # TODO: adaptar isso ao intervalo weekly e monthly
             query_occultation = Occultation.objects.filter(
                 date_time__range=[date_start, date_end]
             )
+            print("total query occultation date", query_occultation.values().count())
 
-            #se magmax e magmin foram definidos, filtra por magnitude
+            # se magmax e magmin foram definidos, filtra por magnitude
             magmax = input.get("magnitude_max", None)
             magmin = input.get("magnitude_min", None)
             if magmax and magmin:
                 query_occultation = query_occultation.filter(
                     g_star__range=[magmin, magmax]
                 )
+            print("total query occultation mag", query_occultation.values().count())
 
-            #TODO: Se filter_type foi definido, filtra por nome, classe ou subclasse
-            #TODO: se magdrop foi definido, filtra por magdrop
+            # TODO: Se filter_type foi definido, filtra por nome, classe ou subclasse
+            # TODO: se magdrop foi definido, filtra por magdrop
 
             # se local_solar_time foi definido
             local_solar_time_after = input.get("local_solar_time_after", None)
             local_solar_time_before = input.get("local_solar_time_before", None)
 
             if local_solar_time_after and local_solar_time_before:
+                """ Rodrigo
                 datetime_after = datetime.combine(datetime.min, local_solar_time_after)
-                datetime_before = datetime.combine(datetime.min, local_solar_time_before)
-                time_difference = datetime_before - datetime_after
 
+                datetime_before = datetime.combine(
+                    datetime.min, local_solar_time_before
+                )
+                time_difference = datetime_before - datetime_after
+                print("diferenca", time_difference)
                 if time_difference.seconds >= 0:
                     query_occultation = query_occultation.filter(
                         loc_t__range=[local_solar_time_after, local_solar_time_before]
                     )
+                # if retorna query vazia por isso o erro
+                print("local solar time... ", query_occultation)
+                """
+                """
                 else:
-                    after = Q(loc_t__gte=local_solar_time_after, loc_t__lte=time(23, 59, 59.999999))
-                    before = Q(loc_t__gte=time(0, 0, 0), loc_t__lte=local_solar_time_before)
+                    after = Q(
+                        loc_t__gte=local_solar_time_after,
+                        loc_t__lte=time(23, 59, 59.999999),
+                    )
+                    before = Q(
+                        loc_t__gte=time(0, 0, 0), loc_t__lte=local_solar_time_before
+                    )
                     query_occultation = query_occultation.filter(after | before)
-                    
-            #TODO: se event_duration foi definido
+                    """
+                # Filtro por Solar Time 18:00 - 06:00
+                after = Q(loc_t__gte=time(18, 0, 0), loc_t__lte=time(23, 59, 59))
+                before = Q(loc_t__gte=time(0, 0, 0), loc_t__lte=time(6, 0, 0))
+                query_occultation = query_occultation.filter(Q(after | before))
 
-            #TODO: se diameter_min e diameter_max foram definidos, filtra por diametro
+            print("total query occultation lst", query_occultation.values().count())
+            """"""
+            # TODO: se event_duration foi definido
+            event_duration = input.get("event_duration", None)
 
-            #TODO: filtra por geofiltro (latitude, longitude, location_radius)
+            if event_duration:
+                query_occultation = query_occultation.filter(
+                    event_duration__lte=event_duration
+                )
+            print("total query occultation event", query_occultation.values().count())
 
-            # print(query_occultation.values()[0])
+            # TODO: se diameter_min e diameter_max foram definidos, filtra por diametro
+            diameter_err_min = input.get("diameter_err_min", None)
+            diameter_err_max = input.get("diameter_err_max", None)
+
+            if diameter_err_min and diameter_err_max:
+                query_occultation = query_occultation.filter(
+                    diameter__range=[diameter_err_min, diameter_err_max]
+                )
+            print("total query occultation diam", query_occultation.values().count())
+
+            # TODO: filtra por geofiltro (latitude, longitude, location_radius)
+            occ_path_min_latitude = input.get("occ_path_min_latitude", None)
+
+            if occ_path_min_latitude:
+                query_occultation = query_occultation.filter(
+                    occ_path_min_latitude__lte=occ_path_min_latitude
+                )
+            print("total query occultation lat", query_occultation.values().count())
+
+            # occ_path_min_longitude
+            occ_path_min_longitude = input.get("occ_path_min_longitude", None)
+
+            if occ_path_min_longitude:
+                query_occultation = query_occultation.filter(
+                    occ_path_min_longitude__lte=occ_path_min_longitude
+                )
+            print("total query occultation lon", query_occultation.values().count())
+            #
+            """
+            location_radius = input.get("location_radius", None)
+            if location_radius:
+                query_occultation = query_occultation.filter(
+                    location_radius__lte=location_radius
+                )
+            """
             if query_occultation:
                 return query_occultation
             else:
@@ -97,4 +167,12 @@ class ProcessEventFilters:
         result = self.get_filters()
         for i, r in enumerate(result):
             if i == 0:
-                print(r.keys())
+                print("run_filter...", self.query_occultation(r, "2024-03-12"))#.values())
+                """
+                print(
+                    "run_filter...",
+                    self.query_occultation(r, "2024-03-12").values_list(
+                        "occ_path_coeff", flat=True
+                    ),
+                )
+                """
