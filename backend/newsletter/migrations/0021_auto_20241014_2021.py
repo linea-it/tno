1b789b5e31678e984168a8e4ca40627beb1aca39
josiane-silwa <josiannesilwa@gmail.com>
2024-10-14 20:35:50 +0000

    refactor(backend/newsletter/migrations/0021_auto_20241014_2021.py): Inclusao do campo de incertezas no banco e remoção dos campos de magnitude e magnitude_drop min

--- backend/newsletter/migrations/0021_auto_20241014_2021.py
+++ backend/newsletter/migrations/0021_auto_20241014_2021.py
@@ -6,21 +6,27 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('newsletter', '0020_alter_eventfilter_user'),
+        ("newsletter", "0020_alter_eventfilter_user"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='eventfilter',
-            name='magnitude_drop_min',
+            model_name="eventfilter",
+            name="magnitude_drop_min",
         ),
         migrations.RemoveField(
-            model_name='eventfilter',
-            name='magnitude_min',
+            model_name="eventfilter",
+            name="magnitude_min",
         ),
         migrations.AddField(
-            model_name='eventfilter',
-            name='closest_approach_uncertainty_in_km',
-            field=models.IntegerField(blank=True, default=None, help_text='Uncertainty in geocentric closest approach (km).', null=True, verbose_name='Uncertainty (km)'),
+            model_name="eventfilter",
+            name="closest_approach_uncertainty_in_km",
+            field=models.IntegerField(
+                blank=True,
+                default=None,
+                help_text="Uncertainty in geocentric closest approach (km).",
+                null=True,
+                verbose_name="Uncertainty (km)",
+            ),
         ),
     ]
