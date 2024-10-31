f5d085d6de135536974553aec61559f4a20afdb1
josiane-silwa <josiannesilwa@gmail.com>
2024-10-31 13:10:18 +0000

    refactor(backend/newsletter/management/commands/execute_subscription_filter.py): Inclusão da execução da função que envia o email para o usuario com os resutados encontrados

--- backend/newsletter/management/commands/execute_subscription_filter.py
+++ backend/newsletter/management/commands/execute_subscription_filter.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timezone
 
 from django.core.management.base import BaseCommand
+from newsletter.models.event_filter import EventFilter
+from newsletter.models.subscription import Subscription
+from newsletter.newsletter_send_mail import NewsletterSendEmail
 from newsletter.process_event_filter import ProcessEventFilters
 
 
@@ -13,11 +16,33 @@ class Command(BaseCommand):
             action="store_true",
             dest="local",
             default=False,
-            help="Does not download the files, uses the local files in /data/asteroid_table",
+            help="Dispara a função que executa os filtros.",
         )
 
-    def handle(self, *args, **options):
-        atm = ProcessEventFilters(stdout=True)
+        # parser.add_argument(
+        #    "email",
+        #    help="One email addresses to send a test email to.",
+        # )
+
+    def handle(self, *args, **kwargs):
+        # atm = ProcessEventFilters(stdout=True)
         # passa o periodo no parametro
         # 1 para monthly, 2 para weekly
-        atm.run_filter(1)
+        # atm.run_filter(1)
+
+        # email = kwargs["email"]
+
+        user_subs_all = len(EventFilter.objects.values_list("user", flat=True))
+        print(user_subs_all)
+
+        for u in range(user_subs_all):
+            user_subs = EventFilter.objects.values_list("user", flat=True)[u]
+            # print("user_subs", user_subs)
+
+            obj = EventFilter.objects.filter(user=user_subs)[0]
+
+            email_user = obj.user.email
+            print(f"Subscription ID: {obj.pk} Email: {obj.user.email}")
+
+            send_mail = NewsletterSendEmail()
+            send_mail.send_events_mail(obj.pk, email=email_user)
