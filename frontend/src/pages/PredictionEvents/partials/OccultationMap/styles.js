1e7705508b479c29d863de8a90fc7e7da9c40ef2
josiane-silwa <josiannesilwa@gmail.com>
2024-12-16 13:58:30 +0000

    Inclusao do componente de mapas react-leaflet

---
+++ frontend/src/pages/PredictionEvents/partials/OccultationMap/styles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from '@mui/styles'
+
+const styles = makeStyles((theme) => ({
+  map: {
+    height: '80vh ',
+    width: '100% ',
+    borderRadius: '10px '
+  },
+
+  mapThumbsCard: {
+    height: '175px ',
+    width: '190px ',
+    borderRadius: '10px '
+  },
+
+  mapThumbsList: {
+    height: '72px ',
+    width: '93px ',
+    borderRadius: '10px '
+  }
+}))
+
+export default styles
