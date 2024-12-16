6b00225701872bc5f0ebbe2631e969f762951e9d
josiane-silwa <josiannesilwa@gmail.com>
2024-12-16 18:06:46 +0000

    refactor(frontend/src/pages/PredictionEvents/partials/OccultationMap/index.js): Refatoração e limpeza do componente de mapas

--- frontend/src/pages/PredictionEvents/partials/OccultationMap/index.js
+++ frontend/src/pages/PredictionEvents/partials/OccultationMap/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { useQuery } from 'react-query'
 import L from 'leaflet'
-//import { MapContainer, TileLayer, Marker, Popup, Polyline, Circle, useMap, useMapEvents } from 'react-leaflet'
 import { MapContainer, TileLayer, Popup, Polyline, Circle, CircleMarker, Marker } from 'react-leaflet'
-//import sun from './data/img/sun.png'
 import star from './data/img/estrela-pontiaguda.png'
 import styles from './styles'
 // css do próprio leaflet, sem ele mapa quebra
@@ -30,26 +28,10 @@ const PredictOccultationMap = ({ occultationId, thumbsCard, thumbsList }) => {
     staleTime: 1 * 60 * 1000
   })
 
-  let state = {
-    //thumbsCard -> mapas menores dos cards grid ,
-    //thumbsList -> mapas pequenos das linhas da lista
-    //ultima opção se refere à pagina de detalhes
-    zoom: thumbsCard === true ? 1 : thumbsList === true ? -1 : 2.6
-  }
-
-  // opções do icone do sol -> falta definir posição real
-  /*
-  const sunIcon = L.icon({
-    iconUrl: sun,
-    //shadowUrl: leafShadow,
-    iconSize: [38, 38], // size of the icon
-    shadowSize: [50, 64], // size of the shadow
-    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62], // the same for the shadow
-    popupAnchor: [-3, -76]
-    //lat: [-47.1729,-10.0068],
-    //lng: [-27.2729,-13.9068],
-  })*/
+  //thumbsCard -> mapas menores dos cards grid ,
+  //thumbsList -> mapas pequenos das linhas da lista
+  //ultima opção se refere à pagina de detalhes
+  let zoomLevel = thumbsCard ? 1 : thumbsList ? -1 : 2.6
 
   // opçoes do icone estrela // falta definir posição real
   const starIcon = L.icon({
@@ -85,46 +67,19 @@ const PredictOccultationMap = ({ occultationId, thumbsCard, thumbsList }) => {
   // linhas azuis
   const blueOptions = { color: 'blue', weight: '1' }
 
-  /// Pega as dimensões do mapa e do zoom
-  /*
-  function MyComponent() {
-    const map = useMapEvents({
-      click: () => {
-        map.locate()
-        //map.setView([50.5, 30.5], map.getZoom())
-        map.setView(position, map.getZoom())
-      },
-      locationfound: (location) => {
-        console.log('location found:', location)
-      }
-    })
-    return null
-  }*/
-
-  //arrays das linhas central upper e lower
-  let lineDown = [],
-    lineCenter = [],
-    pointLineCenter = [],
-    lineUpper = []
-
   const latLonSize = data?.uncertainty_lower_limit_latitude.length
-
   // monta um array [lat, lon] - linhas vermelhas e azul
-  for (let i = 0; i < latLonSize; i++) {
-    lineUpper[i] = [data?.uncertainty_upper_limit_latitude[i], data?.uncertainty_upper_limit_longitude[i]]
-    lineCenter[i] = [data?.central_path_latitude[i], data?.central_path_longitude[i]]
-    lineDown[i] = [data?.uncertainty_lower_limit_latitude[i], data?.uncertainty_lower_limit_longitude[i]]
-  }
+  const lineUpper = data?.uncertainty_upper_limit_latitude?.map((lat, i) => [lat, data.uncertainty_upper_limit_longitude[i]]) || []
+  const lineCenter = data?.central_path_latitude?.map((lat, i) => [lat, data.central_path_longitude[i]]) || []
+  const lineDown = data?.uncertainty_lower_limit_latitude?.map((lat, i) => [lat, data.uncertainty_lower_limit_longitude[i]]) || []
+
+  //arrays das linhas central
+  let pointLineCenter = []
 
   // monta um array [lat, lon] para desenhar os pontos menores
   for (let i = 0; i < latLonSize; i += 64) {
     pointLineCenter[i] = [data?.central_path_latitude[i], data?.central_path_longitude[i]]
   }
-
-  // desenha as sombras mas por enquanto está errado
-  //const shadow = false //true
-
-  //console.log('dataall', data)
 
   return (
     <Card spacing={4}>
@@ -135,33 +90,13 @@ const PredictOccultationMap = ({ occultationId, thumbsCard, thumbsList }) => {
             center={lineUpper[637]}
             zoomControl={thumbsList === true ? false : true}
             attributionControl={thumbsList === true ? false : true}
-            zoom={thumbsCard === true ? 1 : state.zoom}
+            zoom={zoomLevel}
           >
-            {/*maxBounds={[
-              [-90, -360],
-              [90, 360]
-            ]}
-            maxBoundsViscosity={1}*/}
-            {/*<TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            />*/}
             <TileLayer
               url={`https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&hl=en&gl=US&${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
               subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
             />
-            {/*shadow === true ? (
-                <NightRegion
-                  //fillColor='#00345c'
-                  weight='0.01'
-                  color='#001a2e'
-                  refreshInterval={6000} // custom refresh rate in milliseconds, default set to 5000ms
-                />
-              ) : (
-                ''
-              )*/}
 
-            {/*<MyComponent /> // pega dimensoes do zoom map*/}
             <Polyline pathOptions={traceOptions} positions={lineUpper}></Polyline>
             <Polyline pathOptions={blueOptions} positions={lineCenter}></Polyline>
             <Polyline pathOptions={traceOptions} positions={lineDown}></Polyline>
