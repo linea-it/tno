bef6d34cc4419460b37d5404d32647bdde9065bc
josiane-silwa <josiannesilwa@gmail.com>
2024-12-23 19:37:44 +0000

    refactor(frontend/src/pages/PredictionEvents/partials/OccultationMap/NightTime.js): Retirada do tyle com as luzes das cidades. Agora plota somente a sombra uma unica vez

--- frontend/src/pages/PredictionEvents/partials/OccultationMap/NightTime.js
+++ frontend/src/pages/PredictionEvents/partials/OccultationMap/NightTime.js
@@ -8,37 +8,15 @@ import 'leaflet-boundary-canvas'
 const NightLayer = ({ datetime }) => {
   const map = useMap()
 
-  const nightmask = terminator({
-    fillColor: 'gray',
-    weight: 1
-    //color: 'black'
-  }).addTo(map)
-
   // função que atualiza as sombra de acordo com o datetime
   const updateMapTime = (time) => {
+    const nightmask = terminator({
+      fillColor: 'gray',
+      weight: 1
+      //color: 'black'
+    }).addTo(map)
+
     nightmask.setTime(time)
-
-    const nightmaskJson = nightmask.toGeoJSON()
-
-    var nightraster = L.TileLayer.boundaryCanvas(
-      'https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_SNPP_DayNightBand_AtSensor_M15/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}',
-      {
-        boundary: nightmaskJson,
-        attribution:
-          'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
-        bounds: [
-          [-85.0511287776, -179.999999975],
-          [85.0511287776, 179.999999975]
-        ],
-        minZoom: 1,
-        maxZoom: 8,
-        format: 'jpg',
-        time: '',
-        tilematrixset: 'GoogleMapsCompatible_Level'
-      }
-    )
-
-    nightraster.addTo(map)
   }
   updateMapTime(datetime)
 }
