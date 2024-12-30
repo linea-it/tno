8e1fb0a3212d7f85d94a9f8e0bc4d3fe7cd0f5b4
josiane-silwa <josiannesilwa@gmail.com>
2024-12-30 13:33:12 +0000

    refactor(frontend/src/pages/PredictionEvents/partials/OccultationMap/index.js): Separação do componente FlyToMap que move o mapa para a posição especificada com zoom

--- frontend/src/pages/PredictionEvents/partials/OccultationMap/index.js
+++ frontend/src/pages/PredictionEvents/partials/OccultationMap/index.js
@@ -10,19 +10,20 @@ import { getOccultationPaths } from '../../../../services/api/Occultation' // Fu
 import { Typography } from '@mui/material'
 import NightLayer from './NightTime' // componente que desenha as sombras de acordo com o datetime
 import Legend from './Legend' // componente que desenha as lellglendas dinamicamente
+import FlyToMap from './FlyToMap' // componennte que move o mapa para posição especificada
 
 // Componente FlyToMap
 // Responsável por mover progressivamente o mapa para a posição especificada (center) com zoom
-const FlyToMap = ({ center, zoom }) => {
-  const map = useMap() // Obtém a instância do mapa atual
-  useEffect(() => {
-    if (center && zoom) {
-      // Verifica se os parâmetros são válidos
-      map.flyTo(center, zoom, { animate: true, duration: 0.5 }) // Move o mapa com animação
-    }
-  }, [center, zoom, map]) // Efeito dispara quando center ou zoom mudam
-  return null
-}
+// const FlyToMap = ({ center, zoom }) => {
+//   const map = useMap() // Obtém a instância do mapa atual
+//   useEffect(() => {
+//     if (center && zoom) {
+//       // Verifica se os parâmetros são válidos
+//       map.flyTo(center, zoom, { animate: true, duration: 0.5 }) // Move o mapa com animação
+//     }
+//   }, [center, zoom, map]) // Efeito dispara quando center ou zoom mudam
+//   return null
+// }
 
 // // Componente Legend
 // // Adiciona uma legenda dinamicamente ao mapa
