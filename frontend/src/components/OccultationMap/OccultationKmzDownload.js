35d6fb4cdc2cd0cc1d4fd40fbd6f669ebe79d073
josiane-silwa <josiannesilwa@gmail.com>
2025-01-22 18:56:16 +0000

    refactor(frontend/src/components/OccultationMap/OccultationKmzDownload.js): correção do nome das variaveis que contem as coordenadas dos pontos

--- frontend/src/components/OccultationMap/OccultationKmzDownload.js
+++ frontend/src/components/OccultationMap/OccultationKmzDownload.js
@@ -19,7 +19,7 @@ const generateKMZ = (contentString, fileName) => {
 }
 
 const DownloadKMZButton = ({ id, ...params }) => {
-  // gera coordenadas para desenhar os pontos
+  // gera coordenadas para desenhar pontos centrais de 60 passos
   function generateCircle(latCenter, lonCenter, radius, numSegments) {
     let coordinates = []
     const earthRadius = 6371000 // Earth's radius in meters
@@ -31,31 +31,36 @@ const DownloadKMZButton = ({ id, ...params }) => {
 
       const lat = latCenter + (latOffset * 180) / Math.PI
       const lon = lonCenter + (lonOffset * 180) / Math.PI
-      //coordinates.push(`${lon},${lat},0`)
       coordinates.push(`${lon},${lat}`)
     }
     //console.log('coordinates', coordinates)
-    //return coordinates.join(' ')
     return coordinates
   }
 
   // acessando lat lon dos pntos centrais de 60 passos
-  let circleCoordinates = []
+  let pointsCoordinates = []
 
   for (let i = 0; i < params.centralPathSteps.length; i++) {
-    // Example usage //, pontos  menores
     const latCenter = params.centralPathSteps[i][0]
     const lonCenter = params.centralPathSteps[i][1]
     //console.log('lat', latCenter, 'lon', lonCenter)
-    const radius = 20000 // 1000 meters
+    const radius = 10000 // 1000 meters
     const numSegments = 36
 
-    //const circleCoordinates = generateCircle(latCenter, lonCenter, radius, numSegments)
     const newCircleCoordinates = [generateCircle(latCenter, lonCenter, radius, numSegments)]
-    //console.log('newcircleCoordinates', newCircleCoordinates)
-    circleCoordinates.push(...newCircleCoordinates)
+    pointsCoordinates.push(...newCircleCoordinates)
+    //console.log('pointsCoordinates', pointsCoordinates)
   }
-  //console.log('circleCoordinates', circleCoordinates) //.length)
+
+  // gera coordenadas para desenhar o ponto central
+  const latCenter = params.mapCenter[0]
+  const lonCenter = params.mapCenter[1]
+  //console.log('lat', latCenter[0], 'lon', lonCenter[1])
+  const radius = params.diameter != null ? params.diameter : 30000 // 1000 meters
+  const numSegments = 36
+
+  const centralPointCoordinates = [generateCircle(latCenter, lonCenter, radius, numSegments)]
+  //console.log('centralPointCoordinates', centralPointCoordinates)
 
   // função que gera o kml
   const handleDownload = () => {
@@ -69,6 +74,8 @@ const DownloadKMZButton = ({ id, ...params }) => {
       { id: 'Center', lineColor: 'ff0000ff', width: 3, polyColor: 'ff00ff00' },
       { id: 'Other', lineColor: 'ffbbbbff', width: 1, polyColor: 'ffbbbbff' },
       { id: 'Body', lineColor: 'ff00ff00', width: 3, polyColor: 'ff00ff00' }
+      // { id: 'Points', lineColor: 'ff0000ff', width: 28, polyColor: 'ff0000ff' },
+      // { id: 'CentralPoint', lineColor: 'ff0000ff', width: 48, polyColor: ' ' }
     ]
 
     // Adicionando os estilos dinamicamente
@@ -93,8 +100,17 @@ const DownloadKMZButton = ({ id, ...params }) => {
       { name: 'Body shadow limit Lower', style: '#Body', coordinates: params.bodyLower },
       { name: 'Uncertainty Upper', style: '#Rings', coordinates: params.uncertaintyUpper },
       { name: 'Uncertainty Lower', style: '#Rings', coordinates: params.uncertaintyLower }
+      // //{ name: 'Points', style: '#Points', coordinates: pointsCoordinates },
+      // ...pointsCoordinates.map((coords, index) => ({
+      //   name: `Points ${index + 1}`,
+      //   style: '#Points',
+      //   coordinates: coords
+      // })),
+      //{ name: 'Central Point', style: '#CentralPoint', coordinates: centralPointCoordinates }
     ]
 
+    // console.log(params.lineCenter.reverse())
+    // console.log(centralPointCoordinates.reverse())
     // Adicionando as seções dinamicamente
     sections.forEach(({ name, style, coordinates }) => {
       if (coordinates?.length) {
@@ -107,6 +123,9 @@ const DownloadKMZButton = ({ id, ...params }) => {
         contentString += '<altitudeMode>absolute</altitudeMode>\n'
         contentString += '<coordinates>\n'
         coordinates.forEach((coord) => {
+          //  if (name === 'Central Point') {
+          //   console.log(coord)
+          // }
           contentString += `${coord.reverse()},0.0\n`
         })
         contentString += '</coordinates>\n'
@@ -116,9 +135,9 @@ const DownloadKMZButton = ({ id, ...params }) => {
     })
 
     // pontos menores
-    for (let c = 0; c < circleCoordinates.length; c++) {
+    for (let c = 0; c < pointsCoordinates.length; c++) {
       //console.log('contentstring', circleCoordinates[c])
-      let myArrayC = circleCoordinates[c]
+      let myArrayC = pointsCoordinates[c]
       contentString += '<Placemark>\n'
       contentString += '<name>Points</name>\n'
       contentString += '<Style>\n'
@@ -136,12 +155,41 @@ const DownloadKMZButton = ({ id, ...params }) => {
       contentString += '<tessellate>1</tessellate>\n'
       contentString += '<coordinates>\n'
       for (let i = 0; i < myArrayC.length; i++) {
+        //for (let i = 0; i < 30; i++) {
         contentString += myArrayC[i] + ',0.0\n' //[i]
+        console.log(myArrayC[i])
       }
       contentString += '</coordinates>\n'
       contentString += '  </LineString>\n'
       contentString += '</Placemark>\n'
     }
+
+    //ponto central
+    contentString += '<Placemark>\n'
+    contentString += '<name>Central Point</name>\n'
+    contentString += '<Style>\n'
+    //contentString += `<Style id='Points'>\n`
+    contentString += '<LineStyle>\n'
+    contentString += '  <color>ff0000ff</color>\n'
+    contentString += '  <width>48</width>\n'
+    contentString += '</LineStyle>\n'
+    // contentString += '<PolyStyle>\n'
+    // contentString += '<color>ff007cf5</color>\n'
+
+    // contentString += '</PolyStyle>\n'
+    contentString += '  </Style>\n'
+    contentString += '  <LineString>\n'
+    contentString += '<tessellate>1</tessellate>\n'
+    contentString += '<coordinates>\n'
+    let myArrayC = centralPointCoordinates[0]
+    //console.log(myArrayC)
+    for (let i = 0; i < myArrayC.length; i++) {
+      //console.log('myArrayC', myArrayC[i] + ',0.0\n')
+      contentString += myArrayC[i] + ',0.0\n'
+    }
+    contentString += '</coordinates>\n'
+    contentString += '  </LineString>\n'
+    contentString += '</Placemark>\n'
 
     contentString += '</Document> </kml>\n'
 
