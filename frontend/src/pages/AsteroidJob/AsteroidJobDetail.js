b22da21c0c7c61f498c2056ddfb014e8f41d6788
josiane-silwa <josiannesilwa@gmail.com>
2025-02-04 21:13:24 +0000

    refactor(frontend/src/pages/AsteroidJob/AsteroidJobDetail.js): Correção na consulta a api no componente da pagina de detalhes de job dos asteroides

--- frontend/src/pages/AsteroidJob/AsteroidJobDetail.js
+++ frontend/src/pages/AsteroidJob/AsteroidJobDetail.js
@@ -1,18 +1,20 @@
+import React, { useEffect, useState } from 'react'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 import { useParams } from 'react-router-dom'
 import TextField from '@mui/material/TextField'
-import { getAsteroidJobById } from '../../services/api/AsteroidJob'
+import { getAsteroidJobById, getAsteroidJobByIdTwo } from '../../services/api/AsteroidJob'
 import { useQuery } from 'react-query'
 import Alert from '@mui/material/Alert'
 import moment from 'moment'
 import ColumnStatus from '../../components/Table/ColumnStatus'
+
 function AsteroidJob() {
   const { id } = useParams()
-
   const { data, isLoading } = useQuery({
     queryKey: ['asteroidJob', { id }],
-    queryFn: getAsteroidJobById,
+    //queryFn: getAsteroidJobById,
+    queryFn: () => getAsteroidJobById({ queryKey: [{ id }] }),
     keepPreviousData: false,
     refetchInterval: false,
     refetchOnWindowFocus: false,
@@ -22,7 +24,12 @@ function AsteroidJob() {
   })
 
   return (
-    <Grid container spacing={2}>
+    <Grid
+      container
+      spacing={2}
+      justifyContent='center' // Centraliza horizontalmente
+      alignItems='center'
+    >
       {data !== undefined && data.status === 4 && (
         <Grid item xs={12}>
           <Alert severity='error'>{data?.error}</Alert>
@@ -30,18 +37,18 @@ function AsteroidJob() {
       )}
       {isLoading && <span>Loading</span>}
       {!isLoading && (
-        <Grid item xs={12}>
+        <Grid item xs={12} md={6}>
           <Box
             component='form'
             sx={{
-              '& > :not(style)': { m: 1, width: '25ch' }
+              '& > :not(style)': { m: 1, width: '55ch' }
             }}
             noValidate
             autoComplete='off'
           >
             <TextField
               label='ID'
-              value={data?.id}
+              value={data?.id ?? ''}
               InputProps={{
                 readOnly: true
               }}
@@ -49,54 +56,56 @@ function AsteroidJob() {
             <ColumnStatus status={data?.status} />
             <TextField
               label='Submit Time'
-              value={moment(data?.submit_time).utc().format('YYYY-MM-DD HH:mm:ss')}
+              value={data?.submit_time ? moment(data.submit_time).utc().format('YYYY-MM-DD HH:mm:ss') : ''}
               InputProps={{
                 readOnly: true
               }}
             />
             <TextField
               label='Start Time'
-              value={moment(data?.start).utc().format('YYYY-MM-DD HH:mm:ss')}
+              value={data?.start !== null ? moment(data?.start).utc().format('YYYY-MM-DD HH:mm:ss') : ''}
               InputProps={{
                 readOnly: true
               }}
             />
             <TextField
               label='End Time'
-              value={moment(data?.end).utc().format('YYYY-MM-DD HH:mm:ss')}
+              value={data?.end !== null ? moment(data?.end).utc().format('YYYY-MM-DD HH:mm:ss') : ''}
               InputProps={{
                 readOnly: true
               }}
             />
             <TextField
               label='Execution Time'
-              value={data?.exec_time}
+              value={data?.exec_time !== null ? data?.exec_time : ''}
               InputProps={{
                 readOnly: true
               }}
             />
             <TextField
               label='Asteroids Before'
-              value={data?.asteroids_before}
+              value={data?.asteroids_before !== null ? data?.asteroids_before : ''}
               InputProps={{
                 readOnly: true
               }}
             />
             <TextField
               label='Asteroids After'
-              value={data?.asteroids_after}
+              value={data?.asteroids_after !== null ? data?.asteroids_after : ''}
               InputProps={{
                 readOnly: true
               }}
             />
+            {console.log('data asteroid detail', data)}
             <TextField
               label='Traceback'
               value={data?.traceback !== null ? data.traceback : ''}
               multiline
-              rows={4}
+              rows={10}
               InputProps={{
                 readOnly: true
               }}
+              sx={{ width: '400px' }}
             />
           </Box>
         </Grid>
